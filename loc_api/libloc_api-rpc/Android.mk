b4c4f2016ae47fdd00e26b57d1766cf180d6f51a
Mike Lockwood <lockwood@android.com>
2010-01-21 23:49:31 -0500

    Initial commit of Qualcomm's loc_api GPS library for the MSM platform.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

---
+++ loc_api/libloc_api-rpc/Android.mk
@@ -0,0 +1,66 @@
+#Copyright (c) 2009, QUALCOMM USA, INC.
+#All rights reserved.
+#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#·         Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+#·         Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+#·         Neither the name of the QUALCOMM USA, Inc.  nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ifneq ($(BUILD_TINY_ANDROID),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+RPC_INC:=rpc_inc
+
+AMSS_VERSION:=6356
+
+generated_files:= \
+	gen-$(AMSS_VERSION)/loc_api_clnt.c	\
+	gen-$(AMSS_VERSION)/loc_api_cb_xdr.c	\
+	gen-$(AMSS_VERSION)/loc_api_common_xdr.c \
+	gen-$(AMSS_VERSION)/loc_api_cb_svc.c \
+	gen-$(AMSS_VERSION)/loc_api_xdr.c \
+        gen-$(AMSS_VERSION)/loc_api_fixup.c \
+	src/loc_api_rpc_glue.c \
+	src/loc_apicb_appinit.c
+
+LOCAL_SRC_FILES:= $(generated_files)
+
+# removed from library build since the client should implement this code.
+#	src/loc_api_cb_server.c
+
+LOCAL_CFLAGS:=-fno-short-enums
+# LOCAL_CFLAGS+=-include $(RPC_INC)/commondefs.h 
+LOCAL_CFLAGS+=-include $(RPC_INC)/loc_api_common.h
+LOCAL_CFLAGS+=-DDEBUG
+# LOCAL_CFLAGS+=-DDEBUG -DVERBOSE
+
+LOCAL_CFLAGS+=-DADD_XDR_FLOAT -DADD_XDR_BOOL
+
+LOCAL_SHARED_LIBRARIES:= librpc
+LOCAL_STATIC_LIBRARIES:= libcommondefs-rpc
+
+LOCAL_PRELINK_MODULE:= false
+
+LOCAL_COPY_HEADERS_TO:= libloc_api-rpc/$(RPC_INC)
+LOCAL_COPY_HEADERS:= \
+	$(RPC_INC)/debug.h \
+	$(RPC_INC)/loc_api_cb.h \
+	$(RPC_INC)/loc_api_common.h \
+	$(RPC_INC)/loc_api.h \
+	$(RPC_INC)/loc_api_fixup.h \
+	$(RPC_INC)/loc_api_rpc_glue.h \
+	$(RPC_INC)/loc_apicb_appinit.h      		     
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/$(RPC_INC) \
+	$(TARGET_OUT_HEADERS)/libcommondefs-rpc \
+	$(TARGET_OUT_HEADERS)/librpc
+
+LOCAL_MODULE:= libloc_api-rpc
+
+include $(BUILD_STATIC_LIBRARY)
+
+endif
---
+++ loc_api/libloc_api/loc_eng_ni.h
@@ -0,0 +1,56 @@
+/******************************************************************************
+  @file:  loc_eng_ni.cpp
+  @brief:  module for network initiated interactions
+
+  DESCRIPTION
+     LOC_API network initiated operation support
+
+  INITIALIZATION AND SEQUENCING REQUIREMENTS
+
+  -----------------------------------------------------------------------------
+  Copyright (c) 2009 QUALCOMM Incorporated.
+  All Rights Reserved. QUALCOMM Proprietary and Confidential.
+  -----------------------------------------------------------------------------
+******************************************************************************/
+
+/*=====================================================================
+                        EDIT HISTORY FOR MODULE
+
+  This section contains comments describing changes made to the module.
+  Notice that changes are listed in reverse chronological order.
+
+when       who      what, where, why
+--------   ---      -------------------------------------------------------
+07/30/09   dx       Initial version
+
+$Id:
+======================================================================*/
+
+#ifndef LOC_ENG_NI_H
+#define LOC_ENG_NI_H
+
+#include <hardware_legacy/gps_ni.h>
+
+#define LOC_NI_NO_RESPONSE_TIME            20                      /* secs */
+
+extern const GpsNiInterface sLocEngNiInterface;
+
+typedef struct {
+    pthread_t               loc_ni_thread;            /* NI thread */
+    pthread_mutex_t         loc_ni_lock;
+    int                     response_time_left;       /* examine time for NI response */
+    boolean                 notif_in_progress;        /* NI notification/verification in progress */
+    rpc_loc_ni_event_s_type loc_ni_request;
+    int                     current_notif_id;         /* ID to check against response */
+} loc_eng_ni_data_s_type;
+
+// Functions for sLocEngNiInterface
+extern void loc_eng_ni_init(GpsNiCallbacks *callbacks);
+extern void loc_eng_ni_respond(int notif_id, GpsUserResponseType user_response);
+
+extern int loc_eng_ni_callback (
+        rpc_loc_event_mask_type               loc_event,              /* event mask           */
+        const rpc_loc_event_payload_u_type*   loc_event_payload       /* payload              */
+);
+
+#endif /* LOC_ENG_NI_H */
