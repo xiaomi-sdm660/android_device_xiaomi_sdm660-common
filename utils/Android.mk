fed389ab357f49778070b554bfbf254411093c75
Satheesh Jayakumar <sjayakum@codeaurora.org>
2013-05-07 09:31:48 -0700

    GARDEn: porting LE enhancement to Android - fix build error
    Add platform abstraction layer into include path
    
    Change-Id: I1e27a60f3afcc259449d264697bd39c03ef9609f

--- utils/Android.mk
+++ utils/Android.mk
@@ -17,7 +17,9 @@ LOCAL_SRC_FILES += \
     msg_q.c \
     linked_list.c \
     loc_target.cpp \
-    loc_timer.c
+    loc_timer.c \
+    ../platform_lib_abstractions/elapsed_millis_since_boot.cpp
+
 
 LOCAL_CFLAGS += \
      -fno-short-enums \
@@ -26,7 +28,8 @@ LOCAL_CFLAGS += \
 LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
-LOCAL_C_INCLUDES:=
+LOCAL_C_INCLUDES:= \
+    $(LOCAL_PATH)/../platform_lib_abstractions
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
@@ -36,7 +39,11 @@ LOCAL_COPY_HEADERS:= \
    linked_list.h \
    msg_q.h \
    loc_target.h \
-   loc_timer.h
+   loc_timer.h \
+   ../platform_lib_abstractions/platform_lib_includes.h \
+   ../platform_lib_abstractions/platform_lib_time.h \
+   ../platform_lib_abstractions/platform_lib_macros.h
+
 
 LOCAL_MODULE := libgps.utils
 
