c7265bbe1daf6033a7c2b047a5e72c50e032ade6
Dante Russo <drusso@codeaurora.org>
2014-05-07 17:12:49 -0700

    Revert "Avoid GPS build for MSM8916 64bit"
    This reverts commit 0daddfd2fd9f38cce21d604d7d981e8972557740.
    
    Change-Id: I3138bb2d600618cfec5ec7588d5359b15a07cbad

--- utils/Android.mk
+++ utils/Android.mk
@@ -1,4 +1,3 @@
-ifeq (, $(filter aarch64 arm64, $(TARGET_ARCH)))
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 ifneq ($(BUILD_TINY_ANDROID),true)
 #Compile this library only for builds with the latest modem image
@@ -61,4 +60,3 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_SHARED_LIBRARY)
 endif # not BUILD_TINY_ANDROID
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
-endif # not aarch64
