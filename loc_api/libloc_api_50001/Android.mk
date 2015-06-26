87b6a3fff48ca1ba1479dc09b52c0cbbb7892025
Jiafei Wen <jiafeiw@codeaurora.org>
2015-06-26 10:33:49 -0700

    Move the flp definitions to gps-noship
    This change moves those definitions related to FLP form
    gps_extented_c.h to gps-noship.
    
    Change-Id: I3da452fa3e885af509199cae9fe1b1f24ff48100
    CRs-fixed: 801916

--- loc_api/libloc_api_50001/Android.mk
+++ loc_api/libloc_api_50001/Android.mk
@@ -41,7 +41,8 @@ LOCAL_CFLAGS += \
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
-    hardware/qcom/gps/loc_api/libloc_api_50001
+    hardware/qcom/gps/loc_api/libloc_api_50001 \
+    $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
 LOCAL_COPY_HEADERS:= \
@@ -94,7 +95,8 @@ endif
 ## Includes
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    $(TARGET_OUT_HEADERS)/libloc_core
+    $(TARGET_OUT_HEADERS)/libloc_core \
+    $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_RELATIVE_PATH := hw
