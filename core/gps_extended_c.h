99606eb4149baabd9ceda3b5c38939ea65317d78
Dante Russo <drusso@codeaurora.org>
2015-07-02 17:29:55 -0700

    Fix implicit declaration errors in gps hal.
    Including string.h and stdlib.h to fix compiler
    errors
    
    Change-Id: I4e79c71b9e5691e51850adcc52f52b36f8e3d088

--- core/gps_extended_c.h
+++ core/gps_extended_c.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -35,6 +35,8 @@ extern "C" {
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <hardware/gps.h>
 
 /** Location has valid source information. */
--- utils/loc_target.cpp
+++ utils/loc_target.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
