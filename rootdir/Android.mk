5fd30f742d2a73225c700be5ae868137b47c2cce
Bruno Martins <bgcngm@gmail.com>
2016-06-01 15:13:28 +0100

    wayne-common: rootdir: Remove sensors init shell script
    Change-Id: Ice50a9c83a708d7f8300978048cd00c71e88abc8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -150,14 +150,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.sensors.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.sensors.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
