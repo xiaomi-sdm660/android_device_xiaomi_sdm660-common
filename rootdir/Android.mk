e3d6e38894226fa4e003bbe4d88629d4a7f1307c
Sami Tolvanen <samitolvanen@google.com>
2016-06-17 17:26:42 -0700

    sdm660-common: verity: call verity_update_state
    Needed to set up partition.%s.verified properties.
    
    Bug: 29277442
    Change-Id: I295668c454e7e16018e1e4c3bafce5e317616069

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -129,3 +129,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.verity.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.verity.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
