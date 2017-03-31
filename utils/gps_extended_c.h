d09bd9face1c9f1c8b398f49deaeecc3db0d038f
Naresh Munagala <nareshm@codeaurora.org>
2017-03-31 16:31:54 +0530

    add pos tech mask and sv source info
    Change-Id: I069a4564da7b96e29e5305b126cd88db0c426bc4
    CRs-Fixed: 2028470

--- utils/gps_extended_c.h
+++ utils/gps_extended_c.h
@@ -80,10 +80,11 @@ extern "C" {
 /** Raw GNSS position fixes */
 #define ULP_LOCATION_IS_FROM_GNSS_RAW   0X0100
 
+typedef uint32_t LocSvInfoSource;
 /** SVinfo source is GNSS/DR */
-#define ULP_SVINFO_IS_FROM_GNSS         0x0001
+#define ULP_SVINFO_IS_FROM_GNSS       ((LocSvInfoSource)0x0001)
 /** Raw SVinfo from GNSS */
-#define ULP_SVINFO_IS_FROM_GNSS_RAW     0x0002
+#define ULP_SVINFO_IS_FROM_DR         ((LocSvInfoSource)0x0002)
 
 #define ULP_MIN_INTERVAL_INVALID 0xffffffff
 #define ULP_MAX_NMEA_STRING_SIZE 201
@@ -287,6 +288,8 @@ typedef uint16_t GpsLocationExtendedFlags;
 #define GPS_LOCATION_EXTENDED_HAS_GNSS_SV_USED_DATA 0x1000
 /** GpsLocationExtended has valid navSolutionMask */
 #define GPS_LOCATION_EXTENDED_HAS_NAV_SOLUTION_MASK 0x2000
+/** GpsLocationExtended has valid LocPosTechMask */
+#define GPS_LOCATION_EXTENDED_HAS_POS_TECH_MASK   0x4000
 
 typedef uint32_t LocNavSolutionMask;
 /* Bitmask to specify whether SBAS ionospheric correction is used  */
