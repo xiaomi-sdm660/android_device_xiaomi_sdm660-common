9eb12cdecfab845311cb815117ceb74c6c136bd8
jinfaw <jinfaw@codeaurora.org>
2017-05-08 15:45:00 +0800

    sdm660_64:enable TARGET_USES_QTIC
    Re-enable qrdplus after vendor partition was enabled
    
    Change-Id: I9d1dd79e140d0a728a73d8ead91300ca59a99c4e
    CRs-Fixed: 2044401

--- sdm660_64.mk
+++ sdm660_64.mk
@@ -13,7 +13,7 @@ endif
 # Disable QTIC until it's brought up in split system/vendor
 # configuration to avoid compilation breakage.
 ifeq ($(ENABLE_VENDOR_IMAGE), true)
-TARGET_USES_QTIC := false
+#TARGET_USES_QTIC := false
 endif
 
 TARGET_USES_AOSP_FOR_AUDIO := false
