84ef43c6389bbe5f208cfb12d920304110288430
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:58 +0200

    sdm660-common: Remove unused libqsap_sdk
    Change-Id: Ib543425786c498eec8ef421f85047bcb1cb0cae3

--- sdm660.mk
+++ sdm660.mk
@@ -579,7 +579,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    libqsap_sdk \
     libwifi-hal-qcom \
     TetheringConfigOverlay \
     WifiOverlay \
