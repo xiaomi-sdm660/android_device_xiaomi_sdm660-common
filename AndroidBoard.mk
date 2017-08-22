62f7266b7661d1bf568b42ca567cbf3a8ce0c5d5
Ajit Vaishya <ajitv@codeaurora.org>
2017-08-22 14:29:37 +0530

    Use TARGET_OUT_VENDOR to specify the paths for wlan overlay files
    Earlier commit was wrongly referring to the absolute path of wlan
    overlay config files using $(PRODUCT_OUT)/vendor/.
    Replace this with $(TARGET_OUT_VENDOR).
    
    CRs-Fixed: 2096919
    Change-Id: Ibeff85d4245b0a26c2fcd015bb147a3816352037

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -79,7 +79,7 @@ LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/vendor/etc/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -87,7 +87,7 @@ LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/vendor/etc/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
