69cb83d2a464b992edca1471c8cad9fae691a0f5
Chia-I Wu <olv@google.com>
2017-09-28 11:55:45 -0700

    sdm660-common: switch to IMapper 2.1
    Change-Id: I33be48cd40457ceabc24ff5e085fa8ddaa4670e4

--- sdm660.mk
+++ sdm660.mk
@@ -40,7 +40,7 @@ COMMON_PATH := device/xiaomi/sdm660-common
 # Inherit properties
 $(call inherit-product, $(COMMON_PATH)/properties.mk)
 
-# Shims 
+# Shims
 PRODUCT_PACKAGES += \
     camera.sdm660_shim
 
@@ -214,7 +214,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
