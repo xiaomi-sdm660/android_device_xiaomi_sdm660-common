2b14e00707594a6681601933795e4d8ddeb30eaa
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2017-10-10 14:12:48 +0530

    sdm660-common: Enable Thermal HAL 1.0 by default
    Enable Thermal HAL for sdm660_64. It adds Thermal HAL
    to manifest.xml and thermal service to PRODUCT_PACKAGES.
    Change-Id: Id5070101c7a91955c050a9c5c8520cfa43cc8186

--- sdm660.mk
+++ sdm660.mk
@@ -459,7 +459,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
     thermal.sdm660
 
 # ThermalController app
