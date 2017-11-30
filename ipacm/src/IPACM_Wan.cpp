e5254641dd76d5c9b4f19983d93385c79689054e
Mohammed Javid <mjavid@codeaurora.org>
2017-11-30 18:40:52 +0530

    ipacm:Add check to configure default flt rule only once
    If header_partial_default_wan_v4 flag is true
    handle_route_add_evt method is getting called twice and
    default filter rule is installed twice. Add check to
    prevent it.
    
    Change-Id: I9bae2952a63daad72fabc565ef29153a0c0e4c71

--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -756,7 +756,10 @@ void IPACM_Wan::event_callback(ipa_cm_event_id event, void *param)
 #else
 						IPACMDBG_H("adding routing table(upstream), dev (%s) ip-type(%d)\n", dev_name,data->iptype);
 #endif
-						handle_route_add_evt(data->iptype); //sky
+						if (active_v4 == false)
+						{
+							handle_route_add_evt(data->iptype); //sky
+						}
 					}
 #ifdef FEATURE_IPA_ANDROID
 #ifdef FEATURE_IPACM_HAL
@@ -789,7 +792,10 @@ void IPACM_Wan::event_callback(ipa_cm_event_id event, void *param)
 						/* Check & construct STA header */
 						handle_sta_header_add_evt();
 #endif
-						handle_route_add_evt(data->iptype);
+						if (active_v6 == false)
+						{
+							handle_route_add_evt(data->iptype);
+						}
 					}
 #ifdef FEATURE_IPA_ANDROID
 #ifdef FEATURE_IPACM_HAL
