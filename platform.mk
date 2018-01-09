3b3cc4322193e40365fac0a3424e1160d54cadb3
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    sdm660: Uprev soundtrigger to 2.1
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen

--- platform.mk
+++ platform.mk
@@ -95,8 +95,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl:32 \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
