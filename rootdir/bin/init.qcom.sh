62317f4e51fdfcde125809a21d40b650be7c3c1a
Ravi Kishore R <ravikish@codeaurora.org>
2018-01-29 11:23:33 +0530

    sdm660: Remove copying of QCRIL DB from vendor partition
    Remove this redundant mechanism of copying DB.
    QCRIL Daemon already takes care of the copying
    at runtime
    
    Change-Id: I2213c2a8a1c9717c92f3cf5e7873c0ee9e65da35

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -119,14 +119,6 @@ start_msm_irqbalance()
 	fi
 }
 
-start_copying_prebuilt_qcril_db()
-{
-    if [ -f /vendor/radio/qcril_database/qcril.db -a ! -f /data/vendor/radio/qcril.db ]; then
-        cp /vendor/radio/qcril_database/qcril.db /data/vendor/radio/qcril.db
-        chown -h radio.radio /data/vendor/radio/qcril.db
-    fi
-}
-
 baseband=`getprop ro.baseband`
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
@@ -424,12 +416,6 @@ case "$target" in
        esac
         ;;
 esac
-
-#
-# Copy qcril.db if needed for RIL
-#
-start_copying_prebuilt_qcril_db
-echo 1 > /data/vendor/radio/db_check_done
 
 #
 # Make modem config folder and copy firmware config to that folder for RIL
