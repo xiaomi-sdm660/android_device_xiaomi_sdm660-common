abb347287867226bc35d029fd94be63c61a24f42
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    wayne: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
