5318273f3ee28140b6d2b5b640525b09a2ab6ca3
Demon Singur <demonsingur@gmail.com>
2018-03-04 17:25:12 +0000

    sdm660: turn fingerprint wrapper intro a hidl service
    Change-Id: Ib72892417d401947aeb3935ef0f018c3b5daf94b

--- platform.mk
+++ platform.mk
@@ -227,9 +227,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprintd
+    fingerprintd \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
