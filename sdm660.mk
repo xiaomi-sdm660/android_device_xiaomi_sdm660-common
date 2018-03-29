13d1e20c868cd197ec39ed3cd3a3ec4519af7b6a
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-29 20:45:23 +0200

    sdm660-common: build vendor.qti.hardware.camera.device for ims
    Change-Id: I736e19ff5ded1c493d61f6ed400c6decab318c8c

--- sdm660.mk
+++ sdm660.mk
@@ -182,7 +182,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
