25cf313c37f75d6f986af89aa6706bcd9b462d35
Suren Baghdasaryan <surenb@google.com>
2018-03-30 10:29:10 -0700

    sdm660-common: Increase ro.lmk.medium to prevent kills at medium vmpressure
    Setting ro.lmk.medium to 800 to prevent important processes from being
    killed at medium vmpressure levels.
    
    Bug: 77299493
    Test: lmkd_unit_test
    Merged-In: I37e1651a0a2b148a852e5bde86501024d4f87bbf
    Change-Id: I37e1651a0a2b148a852e5bde86501024d4f87bbf
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit c954ec2eb318518cfa5c6f64a688fd8c809e91c5)

--- common_prop.mk
+++ common_prop.mk
@@ -196,7 +196,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.low=1001 \
-    ro.lmk.medium=0 \
+    ro.lmk.medium=800 \
     ro.lmk.critical=0 \
     ro.lmk.critical_upgrade=false \
     ro.lmk.upgrade_pressure=100 \
