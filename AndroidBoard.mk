b3d7f22ea2c0b8cf2afd12301aeea21ce6cafba4
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 21:08:23 +0800

    wayne: Move ramdisk configs to rootdir dir
     * Clean up unused configs
    
    Change-Id: I0aae320468796a67fc2110aef0b31c06da2314e3
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -35,32 +35,6 @@ include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
 
-#----------------------------------------------------------------------
-# Copy additional target-specific files
-#----------------------------------------------------------------------
-include $(CLEAR_VARS)
-LOCAL_MODULE       := vold.fstab
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := fstab_non_AB_variant.qcom
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
 #Create dsp directory
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/dsp)
 
--- device.mk
+++ device.mk
@@ -182,6 +182,11 @@ PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.target.rc \
+    fstab.qcom
+
 # Sensor features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
@@ -204,7 +209,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # FBE support
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
+    $(LOCAL_PATH)/rootdir/bin/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
 
 # MIDI feature
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
