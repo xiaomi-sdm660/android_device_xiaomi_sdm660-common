82457e6bde5634ce2df50a7b28d64c2e2ad58661
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:20:19 +0800

    sdm660: board: Build Snap Camera when available
    Change-Id: Ia31dd12fb8a097ef6b8c8cc14dc098ba5f37ad1c

--- platform.mk
+++ platform.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # WLAN driver configuration file
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
