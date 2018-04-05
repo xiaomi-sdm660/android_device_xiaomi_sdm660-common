d1f7b5ac567627d83c6f521f00f14eac0525655e
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:22:39 +0800

    sdm660: device: Build display Hals for sdm 660
    Change-Id: I8a56f2e7d75c6e8dcc569240656ebf67a5257b14

--- platform.mk
+++ platform.mk
@@ -202,16 +202,16 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
-# WLAN driver configuration file
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(PLATFORM_PATH)/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
+# Display
 PRODUCT_PACKAGES += \
-    wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf
+    copybit.sdm660 \
+    gralloc.sdm660 \
+    hwcomposer.sdm660 \
+    memtrack.sdm660 \
+    liboverlay \
+    libtinyxml
 
-#Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
@@ -227,6 +227,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
     vendor.display.color@1.0-impl
+
+# WLAN driver configuration file
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(PLATFORM_PATH)/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
+
+PRODUCT_PACKAGES += \
+    wpa_supplicant_overlay.conf \
+    p2p_supplicant_overlay.conf
 
 # Vibrator
 PRODUCT_PACKAGES += \
