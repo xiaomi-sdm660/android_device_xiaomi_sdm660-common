533f43e690fd5faef877ba572ef4702f74a18c7b
Kevin Tang <zhikait@codeaurora.org>
2018-04-09 14:32:28 -0700

    Fixed a number of issues with NetworkInfoDataItemBase
    There are a couple of issues. NetworkInfoDataItemBase
    objects might be from OsAgent or GnssLocationProvider.
    The two sources actually have mTypes defined differently.
    In addtion, when there are different types of connections
    such as wifi / mobile, getting connected / disconnected
    independently, clients need to be all notified correctly.
    
    Right now, if mConnected hasn't changed, no updates are
    send. For exmple, if mobile is connected, later wifi
    gets connected too, clients won't know.
    
    SystemStatus is also updated to get updated / colated
    informtion. In the above example, SystemStatus's top
    record would record as both mobile and wifi are connected.
    
    Change-Id: I1825902247fe1d4e6363f5e24a75be7e984d0dc4
    CRs-Fixed: 2221114

--- gnss/XtraSystemStatusObserver.cpp
+++ gnss/XtraSystemStatusObserver.cpp
@@ -69,18 +69,9 @@ bool XtraSystemStatusObserver::updateLockStatus(uint32_t lock) {
     return ( send(LOC_IPC_XTRA, ss.str()) );
 }
 
-bool XtraSystemStatusObserver::updateConnectionStatus(bool connected, int32_t type) {
+bool XtraSystemStatusObserver::updateConnections(uint64_t allConnections) {
     mIsConnectivityStatusKnown = true;
-
-    if (connected) {
-        mConnections.insert(type);
-    } else {
-        if (-1 == type) { // -1 for disconnecting all connections
-            mConnections.clear();
-        } else {
-            mConnections.erase(type);
-        }
-    }
+    mConnections = allConnections;
 
     if (!mReqStatusReceived) {
         return true;
@@ -88,8 +79,7 @@ bool XtraSystemStatusObserver::updateConnectionStatus(bool connected, int32_t ty
 
     stringstream ss;
     ss <<  "connection";
-    ss << " " << (connected ? "1" : "0");
-    ss << " " << type;
+    ss << " " << mConnections;
     return ( send(LOC_IPC_XTRA, ss.str()) );
 }
 
@@ -130,13 +120,6 @@ bool XtraSystemStatusObserver::updateXtraThrottle(const bool enabled) {
     ss <<  "xtrathrottle";
     ss << " " << (enabled ? 1 : 0);
     return ( send(LOC_IPC_XTRA, ss.str()) );
-}
-
-static inline ostream& operator<<(ostream& os, CONNECTIONS& data) {
-    for (auto elem : data) {
-        os << elem << ' ';
-    }
-    return os;
 }
 
 inline bool XtraSystemStatusObserver::onStatusRequested(int32_t xtraStatusUpdated) {
@@ -248,8 +231,7 @@ void XtraSystemStatusObserver::notify(const list<IDataItemCore*>& dlist)
                     {
                         NetworkInfoDataItemBase* networkInfo =
                                 static_cast<NetworkInfoDataItemBase*>(each);
-                        mXtraSysStatObj->updateConnectionStatus(networkInfo->mConnected,
-                                networkInfo->mType);
+                        mXtraSysStatObj->updateConnections(networkInfo->getAllTypes());
                     }
                     break;
 
