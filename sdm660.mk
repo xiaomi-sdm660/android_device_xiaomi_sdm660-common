8ddc3489ac134fc7bdfc6da563c99846bd4d3132
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:55:29 +0800

    sdm660-common: device: Build rcs packages
    Change-Id: Idfc57ee69e8d0d7f35528c80f4ce7286b3501f5e

--- sdm660.mk
+++ sdm660.mk
@@ -354,6 +354,14 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
+# Telephony-ext
 PRODUCT_PACKAGES += \
     ims-ext-common \
     telephony-ext
