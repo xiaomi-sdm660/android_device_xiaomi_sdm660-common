5190107d84299b44df027a2355ad541c3ea6035a
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:30:30 +0800

    sdm660: platform: Build bluetooth related packages
    Change-Id: Ic02040fbb6961a44b7077c98e156012fd9592707

--- platform.mk
+++ platform.mk
@@ -153,6 +153,12 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
+
 #
 # system prop for opengles version
 #
