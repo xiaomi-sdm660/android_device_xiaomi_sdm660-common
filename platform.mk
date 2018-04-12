5a4a81c00ec69ef0f0a68eeadb37f1594c5977df
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:56:06 +0800

    sdm660: platform: Build renderscript hidl hal
    Change-Id: I8bba1a88aef867883daf3b92b13e1361e5ca132a

--- platform.mk
+++ platform.mk
@@ -339,6 +339,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
