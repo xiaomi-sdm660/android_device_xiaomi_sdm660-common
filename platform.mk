18181aa95fb8d79c7fe83c932bee2e19c63ff192
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-12 14:41:42 +0800

    sdm660: platform: Build drm related packages
    Change-Id: Ie522b94ad997dc86858fd4e46fefcb8c4b32df6c

--- platform.mk
+++ platform.mk
@@ -227,6 +227,11 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     android.hardware.configstore@1.0-service
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # FBE support
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/rootdir/bin/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
