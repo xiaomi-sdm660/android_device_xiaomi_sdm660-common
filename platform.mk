cdb891e07fe63949754919708766c49629b818bc
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-17 00:47:33 +0800

    sdm660: platform: Build vendor.qti.hardware.wifi.supplicant@1.0_vendor from source
    Change-Id: I9b0de507f597d54b36ae5194b6b88c050f20663d

--- platform.mk
+++ platform.mk
@@ -401,6 +401,7 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwifi-hal-qcom \
     libwpa_client \
+    vendor.qti.hardware.wifi.supplicant@1.0_vendor \
     wificond \
     wifilogd \
     wpa_supplicant \
