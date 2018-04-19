3300fe6f4f8d6d0b19c1de5c0df096962cae4be2
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-19 13:46:51 +0800

    sdm660: keylayout: Put sdm-snd-card_Button_Jack.kl to correct location
     * Derp...
    
    Change-Id: Ia46285150909cbc219afd8339d9ab120234b1533

--- platform.mk
+++ platform.mk
@@ -258,10 +258,9 @@ PRODUCT_COPY_FILES += $(PLATFORM_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY
     
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/keylayout/sdm630-snd-card_Button_Jack.kl:system/usr/synaptics_dsx/sdm630-snd-card_Button_Jack.kl \
+    $(PLATFORM_PATH)/keylayout/sdm630-snd-card_Button_Jack.kl:system/usr/keylayout/sdm630-snd-card_Button_Jack.kl \
     $(PLATFORM_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(PLATFORM_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
-
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
