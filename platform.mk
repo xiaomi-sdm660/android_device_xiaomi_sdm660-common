227fe640ae42dbcbe836b2d669c751db53365e66
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-22 12:08:42 +0800

    sdm660: audio: Regen audio_effects.xml
     * Using: https://github.com/luk1337/aeffects-conf2xml
    
    Change-Id: I29a5fb0f4e47612ad6a3fddc988772e8e80e76b1

--- platform.mk
+++ platform.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(PLATFORM_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(PLATFORM_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(PLATFORM_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(PLATFORM_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
