c851793cc5c5cb50f39980b75226c36a05f7f7b2
guaiyihu <guaiyihu@foxmail.com>
2018-05-09 17:39:41 +0800

    sdm660: device: Build android.hidl.manager@1.0-java
    Change-Id: I0a5dc836d0e97ca761be3a3612c5e1d66adb3164

--- platform.mk
+++ platform.mk
@@ -252,7 +252,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # IMS
 PRODUCT_PACKAGES += \
