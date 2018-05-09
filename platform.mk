282fe4ae593e573b16e8503b6683ee169b69554f
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-09 13:16:27 -0700

    sdm660: build vendor version of libstdc++
    Change-Id: Ie43c0a6b594ecfcff393efb8a3937f04af0ee0a4

--- platform.mk
+++ platform.mk
@@ -431,7 +431,8 @@ PRODUCT_COPY_FILES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-     vndk_package
+	libstdc++.vendor \
+	vndk_package
 
 PRODUCT_COPY_FILES += \
 	prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
