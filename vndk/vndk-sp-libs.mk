cd6d111783d8d3cedc8465821bbf5ea7cb3f31b9
guaiyihu <guaiyihu@foxmail.com>
2018-05-22 12:30:41 +0800

    sdm660: vndk: Add android.hidl.base@1.0 to vndk sp library list
    Change-Id: I862eecbf4b41b8aed20f224522027763bf3eafdb

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -3,6 +3,7 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.common@1.0 \
     android.hardware.renderscript@1.0 \
+    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
     libRSDriver \
