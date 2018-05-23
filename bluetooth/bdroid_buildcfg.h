f78ff8405230712f4b70e4b9b768cb3d8fe45b5d
Isaac Chen <isaacchen@isaacchen.cn>
2018-05-23 08:50:16 +0200

    wayne: Undef BTM_DEF_LOCAL_NAME
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,6 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "MI 6X"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
