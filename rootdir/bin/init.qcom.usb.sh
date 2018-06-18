7694b15bde7bfbb9cb55aa51fe794b069a5e2117
guaiyihu <guaiyihu@foxmail.com>
2018-06-18 17:03:14 +0800

    sdm660-common: rootdir: Set MTP product name dynamically
    Change-Id: I3af1f3ea3bd15a20e7623fcb5078e585619c1564

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -150,12 +150,15 @@ fi
 
 # check configfs is mounted or not
 if [ -d /config/usb_gadget ]; then
-	# Chip-serial is used for unique MSM identification in Product string
-	msm_serial=`cat /sys/devices/soc0/serial_number`;
-	msm_serial_hex=`printf %08X $msm_serial`
-	machine_type=`cat /sys/devices/soc0/machine`
-	product_string="$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
-	echo "$product_string" > /config/usb_gadget/g1/strings/0x409/product
+	product_string=`cat /config/usb_gadget/g1/strings/0x409/product` 2> /dev/null
+	if [ "product_string" == "" ]; then
+		# Chip-serial is used for unique MSM identification in Product string
+		msm_serial=`cat /sys/devices/soc0/serial_number`;
+		msm_serial_hex=`printf %08X $msm_serial`
+		machine_type=`cat /sys/devices/soc0/machine`
+		product_string="$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
+		echo "$product_string" > /config/usb_gadget/g1/strings/0x409/product
+	fi
 
 	# ADB requires valid iSerialNumber; if ro.serialno is missing, use dummy
 	serialnumber=`cat /config/usb_gadget/g1/strings/0x409/serialnumber 2> /dev/null`
