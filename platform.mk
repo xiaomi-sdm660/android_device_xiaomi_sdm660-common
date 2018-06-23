c1534fdf56b3b421bb65e8635aac75ca6de82502
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    sdm660: Build separate system and vendor hidl libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- platform.mk
+++ platform.mk
@@ -221,8 +221,10 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0_system
 
 # IMS
 PRODUCT_PACKAGES += \
