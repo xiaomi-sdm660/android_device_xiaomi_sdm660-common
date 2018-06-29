b7caae3d079644b47916b36db0d786eb11dc5ff1
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    wayne: Remove camera.device impl from PRODUCT_PACKAGES
    * camera.device impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- device.mk
+++ device.mk
@@ -142,9 +142,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
