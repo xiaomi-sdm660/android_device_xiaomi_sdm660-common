255bb3b8c280650d35e7e78f9a4024a0f5ca8a43
Bruno Martins <bgcngm@gmail.com>
2018-07-02 21:27:58 +0300

    sdm660: charger:  Use MIUI poweroff charging animation
    * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    https://review.lineageos.org/#/c/LineageOS/android_device_xiaomi_msm8996-common/+/205385/
    
    Change-Id: I70c6853f54e44591f9c9372066c7216a05132eaa

--- platform.mk
+++ platform.mk
@@ -237,7 +237,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,3 +13,14 @@ else
 LOCAL_SRC_FILES		:= etc/fstab.qcom
 endif
 include $(BUILD_PREBUILT)
+
+# Offmode charging
+include $(CLEAR_VARS)
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := xiaomi
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)
