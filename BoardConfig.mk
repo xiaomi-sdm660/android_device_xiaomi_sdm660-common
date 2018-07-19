39445a3026aa961bdd372607c2e35168a643b990
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-19 14:04:00 +0200

    wayne: Decleare dt2w node
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,9 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/proc/nvt_wake_gesture"
+
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
