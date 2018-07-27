7b8535343b9962d727bfc91b0e9245e943b70510
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-27 14:27:27 +0200

    wayne: Switch to enforcing
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,6 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
