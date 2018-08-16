7fcc708c1d71abab8bb06a28779dad47b0b3195b
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    wayne-common: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- wayne.mk
+++ wayne.mk
@@ -424,7 +424,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.0-service.basic \
     com.android.future.usb.accessory
 
 # Vibrator
