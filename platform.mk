36ec13f1cdf8214b834f3d0ef26a61d261896cde
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    sdm660: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- platform.mk
+++ platform.mk
@@ -418,7 +418,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service
+	android.hardware.usb@1.0-service.basic
 
 # Video seccomp policy files
 PRODUCT_COPY_FILES += \
