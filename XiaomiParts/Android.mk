d60e5ebb8c2dd9ca255f0abb0e60c41121761101
guaiyihu <guaiyihu@foxmail.com>
2018-08-17 21:37:49 +0800

    sdm660: Set LOCAL_PRIVATE_PLATFORM_APIS to true for XiaomiParts and doze
    Change-Id: If290cbcaeeda75b59a4c6558844404ec7ebf5ff6

--- XiaomiParts/Android.mk
+++ XiaomiParts/Android.mk
@@ -15,6 +15,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_USE_AAPT2 := true
 
--- doze/Android.mk
+++ doze/Android.mk
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_USE_AAPT2 := true
 
