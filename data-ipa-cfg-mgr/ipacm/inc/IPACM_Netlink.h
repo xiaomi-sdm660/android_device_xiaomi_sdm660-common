860fcf70989b36f2ce7adc59867e4b09364001c2
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-03 04:07:09 +0000

    sdm660: ipacm: Duplicate sys/socket.h header locally
    IDK why this isn't being properly inheritted
    
    Change-Id: Iaded23ea53bb7ee6d74ad3c2a81d5d780c8cc935
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- data-ipa-cfg-mgr/ipacm/inc/IPACM_Netlink.h
+++ data-ipa-cfg-mgr/ipacm/inc/IPACM_Netlink.h
@@ -61,6 +61,14 @@ extern "C"
 #include <netinet/in.h>
 #include "IPACM_Defs.h"
 
+#define _K_SS_MAXSIZE 128
+#define _K_SS_ALIGNSIZE (__alignof__(struct sockaddr *))
+typedef unsigned short __kernel_sa_family_t;
+struct sockaddr_storage {
+  __kernel_sa_family_t ss_family;
+  char __data[_K_SS_MAXSIZE - sizeof(unsigned short)];
+} __attribute__((aligned(_K_SS_ALIGNSIZE)));
+
 #define MAX_NUM_OF_FD 10
 #define IPA_NL_MSG_MAX_LEN (2048)
 
