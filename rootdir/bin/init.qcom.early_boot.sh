35f48088fba0a8c0c42b6634defa3872804a317e
Anjaneya Prasad Musunuri <aprasad@codeaurora.org>
2018-09-03 13:09:41 +0530

    display: Move node permissions from init to rc file.
    Permissions for Nodes declared in init process will be set during boot time,
    whereas nodes declared in uevent will get during creation of nodes.
    This will save boot up time.
    
    Change-Id: Ie9b423d7f95845930f9a84c20cd52e04051628ce
    CRs-Fixed: 2294045
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -438,55 +438,10 @@ function set_perms() {
     chmod $3 $1
 }
 
-function setHDMIPermission() {
-   file=/sys/class/graphics/fb$1
-   dev_file=/dev/graphics/fb$1
-   dev_gfx_hdmi=/devices/virtual/switch/hdmi
-
-   set_perms $file/hpd system.graphics 0664
-   set_perms $file/res_info system.graphics 0664
-   set_perms $file/vendor_name system.graphics 0664
-   set_perms $file/product_description system.graphics 0664
-   set_perms $file/video_mode system.graphics 0664
-   set_perms $file/format_3d system.graphics 0664
-   set_perms $file/s3d_mode system.graphics 0664
-   set_perms $file/dynamic_fps system.graphics 0664
-   set_perms $file/msm_fb_dfps_mode system.graphics 0664
-   set_perms $file/hdr_stream system.graphics 0664
-   set_perms $file/cec/enable system.graphics 0664
-   set_perms $file/cec/logical_addr system.graphics 0664
-   set_perms $file/cec/rd_msg system.graphics 0664
-   set_perms $file/pa system.graphics 0664
-   set_perms $file/cec/wr_msg system.graphics 0600
-   set_perms $file/hdcp/tp system.graphics 0664
-   set_perms $file/hdmi_audio_cb audioserver.audio 0600
-   set_perms $file/pll_enable system.graphics 0664
-   set_perms $file/hdmi_ppm system.graphics 0664
-
-   ln -s $dev_file $dev_gfx_hdmi
-}
-
 # check for the type of driver FB or DRM
 fb_driver=/sys/class/graphics/fb0
 if [ -e "$fb_driver" ]
 then
-    # check for HDMI connection
-    for fb_cnt in 0 1 2 3
-    do
-        file=/sys/class/graphics/fb$fb_cnt/msm_fb_panel_info
-        if [ -f "$file" ]
-        then
-          cat $file | while read line; do
-            case "$line" in
-                *"is_pluggable"*)
-                 case "$line" in
-                      *"1"*)
-                      setHDMIPermission $fb_cnt
-                 esac
-            esac
-          done
-        fi
-    done
 
     # check for mdp caps
     file=/sys/class/graphics/fb0/mdp/caps
@@ -501,30 +456,6 @@ then
                 esac
         done
     fi
-
-    file=/sys/class/graphics/fb0
-    if [ -d "$file" ]
-    then
-            set_perms $file/idle_time system.graphics 0664
-            set_perms $file/dynamic_fps system.graphics 0664
-            set_perms $file/dyn_pu system.graphics 0664
-            set_perms $file/modes system.graphics 0664
-            set_perms $file/mode system.graphics 0664
-            set_perms $file/msm_cmd_autorefresh_en system.graphics 0664
-    fi
-
-    # set lineptr permissions for all displays
-    for fb_cnt in 0 1 2 3
-    do
-        file=/sys/class/graphics/fb$fb_cnt
-        if [ -f "$file/lineptr_value" ]; then
-            set_perms $file/lineptr_value system.graphics 0664
-        fi
-        if [ -f "$file/msm_fb_persist_mode" ]; then
-            set_perms $file/msm_fb_persist_mode system.graphics 0664
-        fi
-    done
-fi
 
 boot_reason=`cat /proc/sys/kernel/boot_reason`
 reboot_reason=`getprop ro.boot.alarmboot`
