f40c0a7d0f5621bde8c8e9b111df8b876ec5c9f2
Isaac Chen <isaacchen@isaacchen.cn>
2018-09-05 00:53:37 +0800

    Revert "wayne: Update vndk-sp build mechanism and library set."
    This reverts commit 62ca3a07ab64e48bd89377e000e6c540d962b1a2.

--- device.mk
+++ device.mk
@@ -426,10 +426,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
