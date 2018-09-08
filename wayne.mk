33b0e4fac14bf0f4b09b8d46af53e2c0c91cb5a5
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 12:50:26 +0800

    wayne-common: Tweak media profiles
     * Remove unsupported profiles in AOSP from media_profiles_vendor.xml:
       - vga
       - 4kdci
       - timelapsevga
       - timelapse4kdci
       - highspeedcif
       - highspeedvga
       - highspeed4kdci
       - qhd
       - 2k
       - timelapseqhd
       - timelapse2k
       - lpcm
       and use it as media_profiles_V1_0.xml
     * Remove never used CAF media_profiles_V1_0.xml.
    
    Change-Id: I8309af0804caf32ab0870a7ad754b8d41dc9b544
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- wayne.mk
+++ wayne.mk
@@ -302,8 +302,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(COMMON_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+    $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
