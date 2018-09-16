0c0f68bde775a3ad4f19c044c1015ddd66bb0039
Vol Zhdanov <wight554@gmail.com>
2018-09-16 18:32:42 +0000

    sdm660-common: Doze: import resources from common devicesettings

--- doze/Android.mk
+++ doze/Android.mk
@@ -22,8 +22,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
