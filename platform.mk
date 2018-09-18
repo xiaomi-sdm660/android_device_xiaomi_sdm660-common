224f253c28b9a6baf2cd7ddd3cdf91ea946400d9
Max Weffers <rcstar6696@gmail.com>
2018-09-18 12:39:10 +0200

    sdm660: platform: Build vendor.qti.hardware.camera.device
    Change-Id: Ie1824b91f1351b19f88842271ff13858995e7ddf

--- platform.mk
+++ platform.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     libxml2 \
     Snap \
+    vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
 # CNE
