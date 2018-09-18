8d28333c4408a74527c26941192e224c96b3d4fe
Max Weffers <rcstar6696@gmail.com>
2018-09-18 12:35:05 +0200

    sdm660: Add Buildguard for Xiaomi SDM660 devices
    Change-Id: I7e8fd6f88a2bc99cde044e979bf478b5eb4c3108

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),whyred)
+ifeq ($(BOARD_VENDOR_PLATFORM),xiaomi-sdm660)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
