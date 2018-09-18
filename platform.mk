0aec94227db512822d6357361ccc728c36978b3c
Max Weffers <rcstar6696@gmail.com>
2018-09-18 10:53:59 +0200

    sdm660: separte fpc keylayout per device
    Change-Id: I2055bb824ef8455fa74f93e187d7d7274a41a325

--- platform.mk
+++ platform.mk
@@ -273,7 +273,6 @@ PRODUCT_COPY_FILES += $(PLATFORM_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/keylayout/sdm660-snd-card_Button_Jack.kl:system/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
-    $(PLATFORM_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(PLATFORM_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
 # Keymaster HAL
