a77e5c7a18ac1217ea6fde7e69a535d5dddffc0d
GuaiYiHu <guaiyihu@foxmail.com>
2018-09-28 13:52:17 +0800

    wayne-common: wifi: Enable WIFI_HIDL_FEATURE_DUAL_INTERFACE
    See from https://source.android.com/devices/tech/connect/wifi-sta-ap-concurrency
    
    Change-Id: I64f9e7b76e6dffaf59cd78eae4ba3e75f3b13808

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -261,6 +261,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
