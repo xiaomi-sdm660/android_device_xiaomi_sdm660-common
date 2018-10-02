ac06b77d8de9e88bc7e847f79481b9bc562bbee6
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    wayne-common: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- wayne.mk
+++ wayne.mk
@@ -403,7 +403,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # Thermal
 PRODUCT_PACKAGES += \
