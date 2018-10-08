8e3a521a37cf3bab2926713250654c88366ab242
Rizky Benggolo <travarilo@gmail.com>
2018-10-08 10:50:52 +0700

    sdm660: Merge GPS to LA.UM.7.2.r1-04900-sdm660.0
     Conflicts:
    	gps/gnsspps/gnsspps.c
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- gps/gnsspps/gnsspps.c
+++ gps/gnsspps/gnsspps.c
@@ -25,6 +25,7 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include <loc_pla.h>
 #include <log_util.h>
 #include <stdio.h>
 #include <stdlib.h>
