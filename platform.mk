0ff8aa84d47ac76a246e1d5f559b51bf51f3e29a
Max Weffers <rcstar6696@gmail.com>
2018-10-18 16:16:35 +0200

    sdm660: import vibration support per device

--- platform.mk
+++ platform.mk
@@ -307,11 +307,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service \
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom
