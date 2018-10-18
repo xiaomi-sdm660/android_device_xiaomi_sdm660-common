93e37bee62c9a0cb44ccd73507409f84858c522a
Max Weffers <rcstar6696@gmail.com>
2018-10-18 16:02:07 +0200

    sdm660: doze: fix typo in package name
    Change-Id: Id40cc2814edb2b8ea15c480a8f8c6b44667c0160

--- doze/src/org/carbonrom/settings/doze/DozeService.java
+++ doze/src/org/carbonrom/settings/doze/DozeService.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.carbonrom.settings.doze;;
+package org.carbonrom.settings.doze;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
