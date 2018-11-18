6c7db72fa919ffd6d7eaa52c97ce065ee3a6b5c1
Vol Zhdanov <wight554@gmail.com>
2018-11-18 16:35:43 +0000

    sdm660-common: add qcom early boot script
    * to control power off alarm feature
    
    Change-Id: I0d2f5b96f8604d459130aec5f20f87cc75c7bad0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,6 +10,15 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.early_boot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.qcom.early_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
