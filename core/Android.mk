18c99090be9e90ca419d17410de01bca1bdd90b0
haohuang <haohuang@codeaurora.org>
2018-12-15 17:01:37 +0800

    Qualcomm Unified Vendor - GPS
    Remove remaining build flag checks in all
    location vendor modules :
    BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
    LW_FEATURE_SET
    
    Change-Id: Ideacd3ae7f6a49ff3da3ca8cd14668c01400484a
    CRs-Fixed: 2341041

--- core/Android.mk
+++ core/Android.mk
@@ -9,12 +9,6 @@ LOCAL_MODULE := libloc_core
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
-ifeq ($(TARGET_DEVICE),apq8026_lw)
-LOCAL_CFLAGS += -DPDK_FEATURE_SET
-else ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
-LOCAL_CFLAGS += -DPDK_FEATURE_SET
-endif
-
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
