334659fc3bfeb5171a5d3ad6459e9a4e5d0efcab
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    sdm660-common: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- properties.mk
+++ properties.mk
@@ -288,7 +288,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
