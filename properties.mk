351013a6753fabdae493e5b7d8dab1d9bc5da255
Artem Borisov <dedsa2002@gmail.com>
2018-12-17 18:02:34 +0400

    sdm660-common: Disable debug.sf.latch_unsignaled
    Apparently, it introduces dramatical performance degradation in some use cases.
    
    Change-Id: I92aaa4469f6848e4dfff89f3781f31ed527bae2b

--- properties.mk
+++ properties.mk
@@ -68,7 +68,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.renderer=skiagl \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
