4c4eb539fbe3dd754fabd98997a7e481a1754e8b
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    sdm660-common: Enabling sound trigger V2_2 for sdm660 targets
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    Note: V2_1 will be removed in a subsequent cl.
    Test: built android and tested using SoundTriggerTestApp
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- sdm660.mk
+++ sdm660.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
