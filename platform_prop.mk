892bc2c0419b57cb88ad5bfd875eaafc5c9c5dcf
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    sdm845-common: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- platform_prop.mk
+++ platform_prop.mk
@@ -193,6 +193,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
   	persist.mm.enable.prefetch=true \
 	video.disable.ubwc=1
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.qti.sys.fw.bservice_enable=true
+
 # Netflix custom property
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.netflix.bsp_rev=Q660-13149-1
