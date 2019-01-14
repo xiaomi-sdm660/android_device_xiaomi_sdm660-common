51dca017c55fce4852d48ec38fc40b9811ed7fd8
Yingjie Wang <yingjiewang@codeaurora.org>
2019-01-14 16:27:35 +0800

    GNSS HAL 2.0 implementation
    Implement AGnss, AGnssRil, GnssConfiguration,
    GnssMeasurement and MeasurementCorrections of GNSS
    HAL 2.0 to on par with Android P
    
    Change-Id: Id430e60c2d9cc7333f4b331c7e5a1e9a45a0289a
    CRs-Fixed: 2342198

--- android/Android.mk
+++ android/Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifeq ($(GNSS_HIDL_VERSION),2.0)
+include $(LOCAL_PATH)/2.0/Android.mk
+else
 ifeq ($(GNSS_HIDL_VERSION),1.1)
 include $(LOCAL_PATH)/1.1/Android.mk
 else
@@ -7,4 +10,6 @@ include $(LOCAL_PATH)/1.0/Android.mk
 endif
 else #QMAA flag set, build dummy android.hardware.gnss@1.0-impl-qti
 include $(LOCAL_PATH)/dummy/Android.mk
+endif
+
 endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
