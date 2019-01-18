a30584d29073122c46fe508e3ea0b9d547178df2
Max Weffers <rcstar6696@gmail.com>
2019-01-18 13:53:27 +0100

    sdm660: fix typo in cpu name

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2424,7 +2424,7 @@ case "$target" in
             echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
             echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/fast_ramp_down
             chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-            chown system /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+            chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
             # online CPU4
             echo 1 > /sys/devices/system/cpu/cpu4/online
             # configure governor settings for big cluster
