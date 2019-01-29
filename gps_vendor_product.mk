d1537fd7b9ca9448ff17254ad09bbe79cb896d1f
Saurabh Srivastava <ssrivast@codeaurora.org>
2019-01-30 04:07:41 +0530

    Adding gps_vendor_product.mk
    Adding vendor specific make flags
    
    Change-Id: Icbc9bc4ac6acb58e887e142f96ef95742127c215
    CRs-Fixed: 2389780

---
+++ gps_vendor_product.mk
@@ -0,0 +1,10 @@
+# HAL packages
+PRODUCT_PACKAGES += android.hardware.gnss@1.0-impl-qti
+PRODUCT_PACKAGES += android.hardware.gnss@1.0-service-qti
+PRODUCT_PACKAGES += android.hardware.gnss@1.1-impl-qti
+PRODUCT_PACKAGES += android.hardware.gnss@1.1-service-qti
+PRODUCT_PACKAGES += gps.conf
+PRODUCT_PACKAGES += libloc_core
+PRODUCT_PACKAGES += libgnss
+PRODUCT_PACKAGES += liblocation_api
+PRODUCT_PACKAGES += libgps.utils
