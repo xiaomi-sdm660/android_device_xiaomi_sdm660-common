933d7d83bddfa1d82387bbdc2a377cb807948734
Max Weffers <rcstar6696@gmail.com>
2019-03-06 11:28:11 +0100

    sdm660: Add Thermal Controller App

--- platform.mk
+++ platform.mk
@@ -406,6 +406,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.thermal@1.0-service \
 	thermal.sdm660
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+	ThermalController
+
 # Touchscreen
 PRODUCT_PACKAGES += \
 	libtinyxml2
