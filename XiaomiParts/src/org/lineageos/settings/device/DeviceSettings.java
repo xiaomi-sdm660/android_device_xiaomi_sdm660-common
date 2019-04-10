126310743df0a595d800988207b66561a781219d
Stylogey <stylogey@gmail.com>
2019-04-10 21:38:27 +0200

    XiaomiParts: Correct FP actions for swipe gestures

--- XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
+++ XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
@@ -22,6 +22,7 @@ import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 
 import org.lineageos.settings.device.kcal.KCalSettingsActivity;
 import org.lineageos.settings.device.preferences.SecureSettingCustomSeekBarPreference;
@@ -32,34 +33,50 @@ import org.lineageos.settings.device.preferences.VibrationSeekBarPreference;
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    final static String CATEGORY_FP = "fingerprint";
     final static String PREF_ENABLE_FPACTION = "fpaction_enabled";
     final static String PREF_FP_SHUTTER = "fp_shutter";
-    final static String PREF_FPACTION = "fpaction";
+    final static String PREF_FPACTION_UP = "fpaction_up";
+    final static String PREF_FPACTION_DOWN = "fpaction_down";
+    final static String PREF_FPACTION_LEFT = "fpaction_left";
+    final static String PREF_FPACTION_RIGHT = "fpaction_right";
+
     final static String PREF_TORCH_BRIGHTNESS = "torch_brightness";
-    final static String PREF_VIBRATION_STRENGTH = "vibration_strength";
-    // value of vtg_min and vtg_max
-    final static int MIN_VIBRATION = 116;
-    final static int MAX_VIBRATION = 3596;
-    private static final String PREF_ENABLE_HAL3 = "hal3";
-    private static final String CATEGORY_DISPLAY = "display";
-    private static final String PREF_DEVICE_DOZE = "device_doze";
-    private static final String PREF_DEVICE_KCAL = "device_kcal";
-    private static final String PREF_SPECTRUM = "spectrum";
-    private static final String PREF_ENABLE_DIRAC = "dirac_enabled";
-    private static final String PREF_HEADSET = "dirac_headset_pref";
-    private static final String PREF_PRESET = "dirac_preset_pref";
-    private static final String HAL3_SYSTEM_PROPERTY = "persist.camera.HAL3.enabled";
-    private static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
     private final static String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
             "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_0/max_brightness";
     private final static String TORCH_2_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
             "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_1/max_brightness";
+
+    final static String PREF_VIBRATION_STRENGTH = "vibration_strength";
     private final static String VIBRATION_STRENGTH_PATH = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+
+    // value of vtg_min and vtg_max
+    final static int MIN_VIBRATION = 116;
+    final static int MAX_VIBRATION = 3596;
+
+    private static final String PREF_ENABLE_HAL3 = "hal3";
+    private static final String HAL3_SYSTEM_PROPERTY = "persist.camera.HAL3.enabled";
+
+    private static final String CATEGORY_DISPLAY = "display";
+    private static final String PREF_DEVICE_DOZE = "device_doze";
+    private static final String PREF_DEVICE_KCAL = "device_kcal";
+
+    private static final String PREF_SPECTRUM = "spectrum";
+    private static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
+
+    private static final String PREF_ENABLE_DIRAC = "dirac_enabled";
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
     private static final String DEVICE_DOZE_PACKAGE_NAME = "org.lineageos.settings.doze";
+
     private SecureSettingSwitchPreference mEnableHAL3;
     private SecureSettingSwitchPreference mEnableFpAction;
     private SecureSettingSwitchPreference mFpShutter;
-    private SecureSettingListPreference mFpAction;
+    private SecureSettingListPreference mFpActionUp;
+    private SecureSettingListPreference mFpActionDown;
+    private SecureSettingListPreference mFpActionLeft;
+    private SecureSettingListPreference mFpActionRight;
     private SecureSettingCustomSeekBarPreference mTorchBrightness;
     private VibrationSeekBarPreference mVibrationStrength;
     private Preference mKcal;
@@ -72,6 +89,8 @@ public class DeviceSettings extends PreferenceFragment implements
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences_xiaomi_parts, rootKey);
 
+        String device = FileUtils.getStringProp("ro.build.product", "unknown");
+
         mEnableHAL3 = (SecureSettingSwitchPreference) findPreference(PREF_ENABLE_HAL3);
         mEnableHAL3.setChecked(FileUtils.getProp(HAL3_SYSTEM_PROPERTY, false));
         mEnableHAL3.setOnPreferenceChangeListener(this);
@@ -82,9 +101,29 @@ public class DeviceSettings extends PreferenceFragment implements
         mFpShutter = (SecureSettingSwitchPreference) findPreference(PREF_FP_SHUTTER);
         mFpShutter.setOnPreferenceChangeListener(this);
 
-        mFpAction = (SecureSettingListPreference) findPreference(PREF_FPACTION);
-        mFpAction.setSummary(mFpAction.getEntry());
-        mFpAction.setOnPreferenceChangeListener(this);
+        mFpActionUp = (SecureSettingListPreference) findPreference(PREF_FPACTION_UP);
+        mFpActionUp.setSummary(mFpActionUp.getEntry());
+        mFpActionUp.setOnPreferenceChangeListener(this);
+
+        mFpActionDown = (SecureSettingListPreference) findPreference(PREF_FPACTION_DOWN);
+        mFpActionDown.setSummary(mFpActionDown.getEntry());
+        mFpActionDown.setOnPreferenceChangeListener(this);
+
+        mFpActionLeft = (SecureSettingListPreference) findPreference(PREF_FPACTION_LEFT);
+        mFpActionLeft.setSummary(mFpActionLeft.getEntry());
+        mFpActionLeft.setOnPreferenceChangeListener(this);
+
+        mFpActionRight = (SecureSettingListPreference) findPreference(PREF_FPACTION_RIGHT);
+        mFpActionRight.setSummary(mFpActionRight.getEntry());
+        mFpActionRight.setOnPreferenceChangeListener(this);
+
+        if (device.equals("clover")) {
+            PreferenceCategory fpCategory = (PreferenceCategory) findPreference(CATEGORY_FP);
+            fpCategory.removePreference(mFpActionUp);
+            fpCategory.removePreference(mFpActionLeft);
+            fpCategory.removePreference(mFpActionRight);
+            mFpActionDown.setTitle(R.string.fpaction_title);
+        }
 
         mTorchBrightness = (SecureSettingCustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
         mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&
@@ -140,9 +179,24 @@ public class DeviceSettings extends PreferenceFragment implements
                 FileUtils.setProp(HAL3_SYSTEM_PROPERTY, (Boolean) value);
                 break;
 
-            case PREF_FPACTION:
-                mFpAction.setValue((String) value);
-                mFpAction.setSummary(mFpAction.getEntry());
+            case PREF_FPACTION_UP:
+                mFpActionUp.setValue((String) value);
+                mFpActionUp.setSummary(mFpActionUp.getEntry());
+                break;
+
+            case PREF_FPACTION_DOWN:
+                mFpActionDown.setValue((String) value);
+                mFpActionDown.setSummary(mFpActionDown.getEntry());
+                break;
+
+            case PREF_FPACTION_LEFT:
+                mFpActionLeft.setValue((String) value);
+                mFpActionLeft.setSummary(mFpActionLeft.getEntry());
+                break;
+
+            case PREF_FPACTION_RIGHT:
+                mFpActionRight.setValue((String) value);
+                mFpActionRight.setSummary(mFpActionRight.getEntry());
                 break;
 
             case PREF_TORCH_BRIGHTNESS:
--- keyhandler/src/org/lineageos/settings/device/KeyHandler.java
+++ keyhandler/src/org/lineageos/settings/device/KeyHandler.java
@@ -26,10 +26,18 @@ import com.android.internal.os.DeviceKeyHandler;
 
 public class KeyHandler implements DeviceKeyHandler {
 
-    private static final int SCANCODE_JASMINE = 108;
-    private static final int SCANCODE_CLOVER = 172;
+    private static final int SCANCODE_JASMINE_UP = 103;
+    private static final int SCANCODE_JASMINE_DOWN = 108;
+    private static final int SCANCODE_JASMINE_LEFT = 105;
+    private static final int SCANCODE_JASMINE_RIGHT = 106;
 
-    private static int sActionBefore = KeyEvent.ACTION_DOWN;
+    private static final int SCANCODE_CLOVER_DOWN = 172;
+
+    private static final String FP_SWIPE_DIRECTION = "FP_SWIPE_DIRECTION";
+    private static final int FP_SWIPE_UP = 0;
+    private static final int FP_SWIPE_DOWN = 1;
+    private static final int FP_SWIPE_LEFT = 2;
+    private static final int FP_SWIPE_RIGHT = 3;
 
     private static PackageManager sPackageManager;
     private Context mContext;
@@ -44,19 +52,42 @@ public class KeyHandler implements DeviceKeyHandler {
         int scanCode = event.getScanCode();
         int action = event.getAction();
 
-        if (scanCode == SCANCODE_JASMINE || scanCode == SCANCODE_CLOVER) {
-            if (sActionBefore != KeyEvent.ACTION_UP) {
-                if (action == KeyEvent.ACTION_UP) {
-                    Intent startAction = new Intent()
-                            .setComponent(new ComponentName("org.lineageos.settings.device",
-                                    "org.lineageos.settings.device.StartAction"));
+        int swipeDirection = -1;
 
-                    if (startAction.resolveActivity(sPackageManager) != null) {
-                        mContext.startService(startAction);
-                    }
+        switch (scanCode) {
+            case SCANCODE_JASMINE_UP:
+                swipeDirection = FP_SWIPE_UP;
+                break;
+
+            case SCANCODE_CLOVER_DOWN:
+            case SCANCODE_JASMINE_DOWN:
+                swipeDirection = FP_SWIPE_DOWN;
+                break;
+
+            case SCANCODE_JASMINE_LEFT:
+                swipeDirection = FP_SWIPE_LEFT;
+                break;
+
+            case SCANCODE_JASMINE_RIGHT:
+                swipeDirection = FP_SWIPE_RIGHT;
+                break;
+
+            default:
+                swipeDirection = -1;
+                break;
+        }
+
+        if (swipeDirection >= 0) {
+            if (action == KeyEvent.ACTION_UP) {
+                Intent startAction = new Intent()
+                        .setComponent(new ComponentName("org.lineageos.settings.device",
+                                "org.lineageos.settings.device.StartAction"))
+                        .putExtra(FP_SWIPE_DIRECTION, swipeDirection);
+
+                if (startAction.resolveActivity(sPackageManager) != null) {
+                    mContext.startService(startAction);
                 }
             }
-            sActionBefore = action;
         }
         return event;
     }
