c6601ce9620bd078ae6e5727cdf123b72bd5294d
Manish4586 <Manish.n.manish45@gmail.com>
2019-05-23 13:14:30 +0530

    sdm660-common: platform: fix errors due to embedded
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- platform.mk
+++ platform.mk
@@ -28,7 +28,7 @@ PLATFORM_PATH := device/xiaomi/sdm660-common
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product-if-exists, build/target/product/embedded.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
 # Overlays
