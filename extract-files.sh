d581b8bbe75dd03542359dfb2dbc9a58aedb7260
Max Weffers <rcstar6696@gmail.com>
2019-06-02 08:25:25 +0200

    sdm660: scripts: patchelf mlipay blobs dependencies

--- extract-files.sh
+++ extract-files.sh
@@ -75,4 +75,10 @@ if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
             "${KANG}" --section "${SECTION}"
 fi
 
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+
+patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$BLOB_ROOT"/vendor/bin/mlipayd@1.1
+patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$BLOB_ROOT"/vendor/lib64/libmlipay.so
+patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "$BLOB_ROOT"/vendor/lib64/libmlipay@1.1.so
+
 "${MY_DIR}/setup-makefiles.sh"
