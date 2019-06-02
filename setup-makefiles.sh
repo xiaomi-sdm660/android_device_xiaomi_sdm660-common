ab876fa8d719da367d0f4ff679862399ecae9bea
Max Weffers <rcstar6696@gmail.com>
2019-06-02 10:13:51 +0200

    sdm660: scripts: add correct device names to extract and makefile script

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -37,7 +37,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "capricorn gemini lithium natrium scorpio"
+write_headers "clover jasmine_sprout wayne"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
