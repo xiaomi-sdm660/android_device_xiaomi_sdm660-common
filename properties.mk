a801241cedfff59006dc668e61d18a3730e5730e
Wang Han <416810799@qq.com>
2019-06-05 21:46:35 +0800

    sdm660-common: Enable display default color mode
     * Though xiaomi disables this to make vendor color adjustments,
       livedisplay needs this to apply color profiles on boot.
    
    Change-Id: Ia47d0f2665dc126388be64848a13e2046ab8f258

--- properties.mk
+++ properties.mk
@@ -150,7 +150,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.video.disable.ubwc=1 \
-    video.disable.ubwc=1
+    video.disable.ubwc=1 \
+    vendor.display.enable_default_color_mode=0
 
 # enable FIFO scheduling for UI and Render threads by default
 PRODUCT_PROPERTY_OVERRIDES += \
