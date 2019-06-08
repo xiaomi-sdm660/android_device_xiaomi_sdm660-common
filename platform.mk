3a45a353d6a8bdc580a0c7587d83b25e5649c755
Max Weffers <rcstar6696@gmail.com>
2019-06-08 10:22:42 +0200

    sdm660: plastform: Drop FM radio packages

--- platform.mk
+++ platform.mk
@@ -164,14 +164,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# FM packages
-PRODUCT_PACKAGES += \
-	libqcomfm_jni \
-	android.hardware.broadcastradio@1.0-impl \
-	FM2 \
-	qcom.fmradio \
-	qcom.fmradio.xml
-
 # Framework RRO
 PRODUCT_ENFORCE_RRO_TARGETS := \
 	framework-res
