79cfa0e70999cf341de046e3567d80ba82586741
Max Weffers <rcstar6696@gmail.com>
2019-07-13 08:52:12 +0200

    Revert "sdm660-common: power-libperfmgr: InteractionHandler: exit silently if int state is uninitialized"
    This reverts commit f3f0eebf1587f4ea13025a89b4f2a2c2c3640004.

--- power-libperfmgr/InteractionHandler.cpp
+++ power-libperfmgr/InteractionHandler.cpp
@@ -118,8 +118,10 @@ void InteractionHandler::Acquire(int32_t duration) {
     ATRACE_CALL();
 
     std::lock_guard<std::mutex> lk(mLock);
-    if (mState == INTERACTION_STATE_UNINITIALIZED)
+    if (mState == INTERACTION_STATE_UNINITIALIZED) {
+        ALOGW("%s: called while uninitialized", __func__);
         return;
+    }
 
     int inputDuration = duration + 650;
     int finalDuration;
