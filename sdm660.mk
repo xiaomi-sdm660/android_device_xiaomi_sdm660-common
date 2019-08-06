a116e28da54bcfe3c6a7c4bce73b7778c55e6857
Max Weffers <rcstar6696@gmail.com>
2019-08-06 20:09:13 +0200

    sdm660-common: Move ConsumerIR HAL to device

--- sdm660.mk
+++ sdm660.mk
@@ -163,14 +163,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
