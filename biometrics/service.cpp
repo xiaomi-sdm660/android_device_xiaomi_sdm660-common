53e8ac411a792431fbc88af1291fa043c65b166a
Max Weffers <rcstar6696@gmail.com>
2019-08-06 21:31:34 +0200

    sdm660-common: Update Fingerprint HIDL HAL

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -17,32 +17,29 @@
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660"
 
 #include <android/log.h>
+#include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
-
+#include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
+#include <android/hardware/biometrics/fingerprint/2.1/types.h>
 #include "BiometricsFingerprint.h"
 
-// libhwbinder:
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
-
-// Generated HIDL files
 using android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
 using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::sp;
 
 int main() {
-    android::sp<IBiometricsFingerprint> service = BiometricsFingerprint::getInstance();
-
-    if (service == nullptr) {
-        ALOGE("Instance of BiometricsFingerprint is null");
-        return 1;
-    }
+    android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    android::status_t status = service->registerAsService();
-    if (status != android::OK) {
-        ALOGE("Cannot register BiometricsFingerprint service");
-        return 1;
+    if (bio != nullptr) {
+        if (::android::OK != bio->registerAsService()) {
+            return 1;
+        }
+    } else {
+        ALOGE("Can't create instance of BiometricsFingerprint, nullptr");
     }
 
     joinRpcThreadpool();
