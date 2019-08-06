98e758fb4696708c2b20d377b2509fcbe02164be
Max Weffers <rcstar6696@gmail.com>
2019-08-06 21:07:25 +0200

    sdm660-common: Move device specific camera props to device

--- properties.mk
+++ properties.mk
@@ -49,21 +49,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi \
-    persist.vendor.bokeh.switch.lux=290 \
     persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.depth.focus.cb=0 \
     persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.imglib.usefdlite=1 \
-    persist.vendor.camera.isp.clock.optmz=0 \
-    persist.vendor.camera.isp.turbo=1 \
-    persist.vendor.camera.linkpreview=0 \
-    persist.vendor.camera.preview.ubwc=0 \
-    persist.vendor.camera.stats.test=0 \
-    persist.vendor.imx376_ofilm.light.lux=275 \
-    persist.vendor.imx376_ofilm.low.lux=290 \
-    persist.vendor.imx376_sunny.light.lux=275 \
-    persist.vendor.imx376_sunny.low.lux=290 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.qualcomm.qti.qmmi
 
 # Display
