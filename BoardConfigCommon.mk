b84ff14cb133408a152efb02030338d28ab357d3
srfarias <erikofarias@gmail.com>
2019-08-10 01:46:36 -0300

    sdm660-common: boardconfig: disable dexpreopt for prebuilts
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,6 +167,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+			DONT_DEXPREOPT_PREBUILTS := true
       WITH_DEXPREOPT := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
