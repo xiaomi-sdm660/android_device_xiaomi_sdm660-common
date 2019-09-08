607486efe37909feb4c97cdf016952a2c364400a
Arian <arian.kulmer@web.de>
2019-09-08 18:05:06 +0200

    sdm660-common: Uprev clearkey to 1.2

--- sdm660.mk
+++ sdm660.mk
@@ -214,7 +214,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
