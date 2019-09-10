017ca61bf3f8220c5b3af8053e398348bc6d48db
Max Weffers <rcstar6696@gmail.com>
2019-09-10 08:52:07 +0200

    sdm660: add missing libgui symbol for camera hal

--- libshims/camera_sdm660_shim.cpp
+++ libshims/camera_sdm660_shim.cpp
@@ -6,4 +6,7 @@ namespace android {
     extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(uint32_t inUsage, void** vaddr) {
         _ZN7android13GraphicBuffer4lockEjPPvPiS3_(inUsage, vaddr, nullptr, nullptr);
     }
+
+    extern "C" void _ZN7android6Parcel17writeUint64VectorERKNSt3__16vectorIyNS1_9allocatorIyEEEE() { };
+    extern "C" void _ZNK7android6Parcel16readUint64VectorEPNSt3__16vectorIyNS1_9allocatorIyEEEE() { };
 }
