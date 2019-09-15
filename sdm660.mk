4f62e1df407fbfb49fc844f18af4b61dfb62227f
Davide Garberi <dade.garberi@gmail.com>
2019-09-15 17:25:18 +0200

    sdm660-common: Switch to thermal 2.0
    Change-Id: Icb0223aa27866e17ddb1c6763ed1ec15f6b3f0c0

--- sdm660.mk
+++ sdm660.mk
@@ -442,8 +442,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@2.0-service.mock \
     thermal.sdm660
 
 # ThermalController app
