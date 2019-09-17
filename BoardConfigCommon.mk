2ba6705ff654d1abf4290c7f20a805fd6cd3098f
theimpulson <aayushgupta219@gmail.com>
2019-09-17 15:22:44 +0000

    sdm660-common: define TARGET_COPY_OUT_PRODUCT
    Change-Id: I0daa436787db17265f38909ec8c93db40cbafbc0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -238,6 +238,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/bt_firmware:/bt_firmware \
     /mnt/vendor/persist:/persist
 
+TARGET_COPY_OUT_PRODUCT := system/product
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
