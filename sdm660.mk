1a58381571cbc6bc641d3e405cb253f0bf4727db
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-23 15:17:02 +0200

    sdm660: Add missing telephony-utils for qcrilhook

--- sdm660.mk
+++ sdm660.mk
@@ -403,8 +403,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
-		qti-telephony-hidl-wrapper \
-		qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
