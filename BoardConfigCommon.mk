f29f40577cf239376657cb482bd7b97569f25dd4
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    sdm660-common: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -276,7 +276,7 @@ PROTOBUF_SUPPORTED := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # SELinux
-# include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 # BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 # BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 # BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
