3002ad84da301495cb616e502a19dd3c553f2b85
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    sdm660-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5

--- properties.mk
+++ properties.mk
@@ -284,6 +284,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
