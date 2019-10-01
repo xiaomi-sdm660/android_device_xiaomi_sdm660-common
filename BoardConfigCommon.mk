f3b7c8bb637c600df976b03e44e4bce4f10444aa
Max Weffers <rcstar6696@gmail.com>
2019-10-01 13:02:00 +0200

    sdm660: sepolicy: Start Q Bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -277,10 +277,9 @@ BOARD_USES_QC_TIME_SERVICES := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-# BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-# BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-# BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy-minimal
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
 # Telephony
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
