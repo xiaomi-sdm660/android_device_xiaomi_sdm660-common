cd3f612ba7368c10e4e84bcf2f422919d975b75e
Steven Moreland <smoreland@google.com>
2019-10-02 12:49:15 -0700

    sdm660-common: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: Iba2cd20b8b20fac3e7564de6b853b475fd4ebd2a
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- gps/android/2.0/Android.mk
+++ gps/android/2.0/Android.mk
@@ -46,8 +46,6 @@ LOCAL_HEADER_LIBRARIES := \
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libhidlbase \
-    libhidltransport \
-    libhwbinder \
     libcutils \
     libutils \
     android.hardware.gnss@1.0 \
@@ -100,9 +98,7 @@ LOCAL_SHARED_LIBRARIES := \
     libqti_vndfwk_detect \
 
 LOCAL_SHARED_LIBRARIES += \
-    libhwbinder \
     libhidlbase \
-    libhidltransport \
     android.hardware.gnss@1.0 \
     android.hardware.gnss@1.1 \
     android.hardware.gnss@2.0 \
