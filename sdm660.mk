e696cae5c37c46efa2b6c4ed146370755dee29bf
dianlujitao <dianlujitao@lineageos.org>
2019-10-08 19:47:46 +0800

    sdm660: Build cryptfshw HAL
    Change-Id: I2d9730dd38926682edf2f9083ecdf46a2f5dd51d

--- sdm660.mk
+++ sdm660.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # IDC
 PRODUCT_COPY_FILES += \
