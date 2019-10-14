5df7c174cf3369ffb2ed0b08c9a9f84e03850613
UnparallelSKY <55931612+UnparallelSKY@users.noreply.github.com>
2019-10-14 19:00:18 +0200

    sdm660-common: Define first api level in the device tree instead in the common tree
    All sdm660 xiaomi device didn't launched with oreo-mr1 and it will be easer for add other device (jason, lavender, nitrogen, platina, tulip and whyred) who didn't launched with oreo-mr1 in xiaomi-sdm660 org

--- sdm660.mk
+++ sdm660.mk
@@ -24,7 +24,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product-if-exists, build/target/product/embedded.mk)
 
 # Enable updating of APEXes
