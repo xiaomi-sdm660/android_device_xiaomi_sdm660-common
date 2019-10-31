89152ef4454ceb41a4f5b41a96e485d7ce9f63b0
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-10-31 16:52:36 +0000

    sdm660-common: Correct cryptfshw name
    fix encryption, tested on lavender
    
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti
 
 # IDC
 PRODUCT_COPY_FILES += \
