86a5e4b07fdff99c3dc3a29502f833232c82d800
Max Weffers <rcstar6696@gmail.com>
2019-11-02 18:38:17 +0100

    sdm660: prop: skia hwui renderer is not used on Q anymore

--- properties.mk
+++ properties.mk
@@ -137,7 +137,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.renderer=skiagl \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
