11d51fd106b018356772e2e25eae8879a1c8c8ee
Stylogey <stylogey@gmail.com>
2019-11-12 17:39:01 +0100

    sdm660-common: Use all surface flinger properties

--- properties.mk
+++ properties.mk
@@ -290,7 +290,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
