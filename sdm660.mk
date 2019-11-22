4adf51fc3330006fe95e44e8fcc903c1d811c922
Bruno Martins <bgcngm@gmail.com>
2019-11-22 17:20:47 +0000

    sdm660: Disable RRO overlays
    Change-Id: Icda8fc7411f59f555dfeef7f1ecacb5a92dd94a6

--- sdm660.mk
+++ sdm660.mk
@@ -440,10 +440,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# RRO
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
