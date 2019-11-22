1c13a08a99e8f1a02901a649810ac15abf17ac00
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    sdm660-common: Switch to Audio HAL V6
    Switch sdm660-common to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on sdm660-common
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- sdm660.mk
+++ sdm660.mk
@@ -80,9 +80,9 @@ endif
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl\
     audio.a2dp.default \
