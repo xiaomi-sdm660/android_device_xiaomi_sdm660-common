262f018a5e2b3272c6bc3dae7989172a49729d2b
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    sdm660-common: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- sdm660.mk
+++ sdm660.mk
@@ -220,7 +220,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Folio
 PRODUCT_PACKAGES += \
