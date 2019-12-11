00a5753be76585ec73cea14c928fd3da4394ed97
Elias Gheeraert <55931612+Mashopy@users.noreply.github.com>
2019-12-11 23:18:45 +0100

    sdm660-common: usb: Set correct name
    We are not oneplus3 afaik

--- usb/Usb.h
+++ usb/Usb.h
@@ -10,7 +10,7 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service.oneplus3"
+#define LOG_TAG "android.hardware.usb@1.0-service.xiaomi_sdm660"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
