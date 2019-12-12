1d6b79cf605b0e56ec44fd0e98e15cf15f9dc47c
Karthi Kandasamy <kartka@codeaurora.org>
2019-12-12 15:28:40 +0530

    sdm660-common: Update display config to version 1.9
     upgrade display config version to 1.9
    
    Change-Id: I29586a29b2091366fa975c2721fe1e507e62a537
    Signed-off-by: c_kartka <kartka@codeaurora.org>

--- sdm660.mk
+++ sdm660.mk
@@ -167,8 +167,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.1_vendor
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # Doze
 PRODUCT_PACKAGES += \
