571868ba4f2fb9b5210cd9e436255469326d3948
nysascape <nysadev@raphielgang.org>
2019-12-14 13:56:40 +0000

    sdm660-common: Support for Platina
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter twolip jasmine_sprout wayne clover lavender,$(TARGET_DEVICE)),)
+ifneq ($(filter twolip jasmine_sprout wayne clover lavender platina,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 
