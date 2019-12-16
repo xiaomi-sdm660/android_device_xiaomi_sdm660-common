5eeebf42092a5db3ea020eb6acbe09aa73f9c707
Anant Goel <anantg@codeaurora.org>
2019-12-16 11:45:01 -0800

    init.qcom.usb.sh: Set ADB as default composition for GVM targets
    Set ADB as the default composition for GVM targets, as DIAG
    over USB conflicts with the DIAG over IP used by the PVM.
    
    Change-Id: I09389564db284b16ccb34243eaf34bb17860b30f
    Signed-off-by: Anant Goel <anantg@codeaurora.org>

--- rootdir/bin/init.qcom.usb.sh
+++ rootdir/bin/init.qcom.usb.sh
@@ -42,6 +42,8 @@ soc_id=`cat /sys/devices/soc0/soc_id 2> /dev/null`
 esoc_name=`cat /sys/bus/esoc/devices/esoc0/esoc_name 2> /dev/null`
 
 target=`getprop ro.board.platform`
+product=`getprop ro.product.name`
+product=${product:(-4)}
 
 if [ -f /sys/class/android_usb/f_mass_storage/lun/nofua ]; then
 	echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
@@ -68,7 +70,11 @@ if [ "$(getprop persist.vendor.usb.config)" == "" -a \
                   *)
 		  case "$soc_machine" in
 		    "SA")
-	              setprop persist.vendor.usb.config diag,adb
+			if [ "$product" == "gvmq" ]; then
+				setprop persist.vendor.usb.config adb
+			else
+				setprop persist.vendor.usb.config diag,adb
+			fi
 		    ;;
 		    *)
 	            case "$target" in
