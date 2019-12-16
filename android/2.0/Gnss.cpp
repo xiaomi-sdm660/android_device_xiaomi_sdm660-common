ed3ccee180a8954315cb6e41955234332c1c4d0d
Kevin Tang <zhikait@codeaurora.org>
2019-12-16 15:09:07 -0800

    stub out injectTime API as we should not handle that
    We should no longer accept the NTP injection from AFW
    as we have our XC handling the NTP update.
    
    Change-Id: I2a4c4da4d4b7c46c8ebe70f0666b2fd8ad1a6a5f
    CRs-Fixed: 2595204

--- android/2.0/Gnss.cpp
+++ android/2.0/Gnss.cpp
@@ -330,14 +330,7 @@ Return<bool> Gnss::injectLocation(double latitudeDegrees,
 
 Return<bool> Gnss::injectTime(int64_t timeMs, int64_t timeReferenceMs,
                               int32_t uncertaintyMs) {
-    ENTRY_LOG_CALLFLOW();
-    const GnssInterface* gnssInterface = getGnssInterface();
-    if (nullptr != gnssInterface) {
-        gnssInterface->injectTime(timeMs, timeReferenceMs, uncertaintyMs);
-        return true;
-    } else {
-        return false;
-    }
+    return false;
 }
 
 Return<void> Gnss::deleteAidingData(V1_0::IGnss::GnssAidingData aidingDataFlags)  {
