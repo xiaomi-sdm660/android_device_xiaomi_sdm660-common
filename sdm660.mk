32528d64fb9c850ff3c9e8464ef6e360451ffb60
Max Weffers <rcstar6696@gmail.com>
2020-01-02 13:00:15 +0100

    sdm660: Build Snap Cam

--- sdm660.mk
+++ sdm660.mk
@@ -116,7 +116,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    Snap
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
