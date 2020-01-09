d3ddf866f65c7c698b025a0f1a06aa4bd1d433f7
dianlujitao <dianlujitao@lineageos.org>
2020-01-09 20:46:29 +0800

    sdm660-common: Add ifaa apis to unrestricted greylist
    Change-Id: If33a18edaf6083281265ce94fd143d72b4bb4fb8

--- org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV4.java
+++ org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV4.java
@@ -1,7 +1,12 @@
 package org.ifaa.android.manager;
 
+import android.annotation.UnsupportedAppUsage;
+
 public abstract class IFAAManagerV4 extends IFAAManagerV3 {
+    @UnsupportedAppUsage
     public abstract int getEnabled(int bioType);
 
+    @UnsupportedAppUsage
     public abstract int[] getIDList(int bioType);
+
 }
