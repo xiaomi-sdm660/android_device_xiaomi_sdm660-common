58fcc01265e8cd796a7eae60f4dd5d17d27f5eaa
Max Weffers <rcstar6696@gmail.com>
2020-01-13 16:48:45 +0100

    sdm660: scripts: Adapt for Carbon Rom
    Change-Id: Idaf59c07784c9f94f52f2cea56a9567f1cc8b615

--- extract-files.sh
+++ extract-files.sh
@@ -26,7 +26,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/carbon/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
