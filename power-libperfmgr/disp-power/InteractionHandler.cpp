2e52f7286a3ff8884b92476193711671823d911f
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 20:43:22 +0530

    sdm660-common: power-libperfmgr: Remove ATRACE instances
    Change-Id: I50eaf5fd6c3763ab005ec965c3dfa173808313e3

--- power-libperfmgr/disp-power/InteractionHandler.cpp
+++ power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -15,7 +15,6 @@
  */
 
 #define LOG_TAG "android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr"
-#define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>
 #include <poll.h>
@@ -105,7 +104,6 @@ void InteractionHandler::PerfLock() {
     if (!mHintManager->DoHint("INTERACTION")) {
         ALOGE("%s: do hint INTERACTION failed", __func__);
     }
-    ATRACE_INT("interaction_lock", 1);
 }
 
 void InteractionHandler::PerfRel() {
@@ -113,7 +111,6 @@ void InteractionHandler::PerfRel() {
     if (!mHintManager->EndHint("INTERACTION")) {
         ALOGE("%s: end hint INTERACTION failed", __func__);
     }
-    ATRACE_INT("interaction_lock", 0);
 }
 
 size_t InteractionHandler::CalcTimespecDiffMs(struct timespec start, struct timespec end) {
@@ -124,7 +121,6 @@ size_t InteractionHandler::CalcTimespecDiffMs(struct timespec start, struct time
 }
 
 void InteractionHandler::Acquire(int32_t duration) {
-    ATRACE_CALL();
 
     std::lock_guard<std::mutex> lk(mLock);
     if (mState == INTERACTION_STATE_UNINITIALIZED) {
@@ -170,7 +166,6 @@ void InteractionHandler::Acquire(int32_t duration) {
 void InteractionHandler::Release() {
     std::lock_guard<std::mutex> lk(mLock);
     if (mState == INTERACTION_STATE_WAITING) {
-        ATRACE_CALL();
         PerfRel();
         mState = INTERACTION_STATE_IDLE;
     } else {
@@ -195,7 +190,6 @@ void InteractionHandler::WaitForIdle(int32_t wait_ms, int32_t timeout_ms) {
     ssize_t ret;
     struct pollfd pfd[2];
 
-    ATRACE_CALL();
 
     ALOGV("%s: wait:%d timeout:%d", __func__, wait_ms, timeout_ms);
 
