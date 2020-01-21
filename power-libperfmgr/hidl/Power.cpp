207ce8c7fddd1ab2d5adb743446d8a49100abc1e
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 20:43:22 +0530

    sdm660-common: power-libperfmgr: Remove ATRACE instances
    Change-Id: I50eaf5fd6c3763ab005ec965c3dfa173808313e3

--- power-libperfmgr/hidl/Power.cpp
+++ power-libperfmgr/hidl/Power.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 #define LOG_TAG "android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr"
 
 #include "Power.h"
@@ -100,7 +99,6 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     if (!mReady) {
         return Void();
     }
-    ATRACE_INT(android::hardware::power::V1_0::toString(hint).c_str(), data);
     ALOGD_IF(hint != PowerHint_1_0::INTERACTION, "%s: %d",
              android::hardware::power::V1_0::toString(hint).c_str(), static_cast<int>(data));
     switch (hint) {
@@ -177,7 +175,6 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
         return Void();
     }
 
-    ATRACE_INT(android::hardware::power::V1_2::toString(hint).c_str(), data);
     ALOGD_IF(hint >= PowerHint_1_2::AUDIO_STREAMING, "%s: %d",
              android::hardware::power::V1_2::toString(hint).c_str(), static_cast<int>(data));
 
@@ -214,7 +211,6 @@ Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
     }
 
     if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
-        ATRACE_INT(android::hardware::power::V1_3::toString(hint).c_str(), data);
         if (mSustainedPerfModeOn) {
             ALOGV("%s: ignoring due to other active perf hints", __func__);
         } else {
