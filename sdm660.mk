419ee63c8f783620034493c803059b73f6371ce6
Alastair Donaldson <afdx@google.com>
2020-02-07 10:29:21 +0000

    sdm660-common: Add android.software.vulkan.deqp.level, version 2020-03-01
    This change sets the new Vulkan dEQP level feature flag to the value
    corresponding to the Vulkan dEQP tests for Android 11.
    
    Change-Id: Ia70f8e432bf8b32420904587c9b45529e4045959
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -447,6 +447,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
 # Power
