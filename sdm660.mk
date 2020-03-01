a8eaf4b5660e2ee94484ed3a241ad40c05bb2c72
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:39:35 -0800

    sdm660-common: Remove unnecessary android.hardware.radio entries
    * These are implicitly included by AOSP.
    
    Change-Id: I9e557444161e7ca60793bddde82330a6bd04908b

--- sdm660.mk
+++ sdm660.mk
@@ -496,9 +496,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
