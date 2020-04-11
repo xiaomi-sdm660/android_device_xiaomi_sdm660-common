8d2864d93456f81142370bfd1328bd7e79b1ef2e
Dušan Uverić <dusan.uveric9@gmail.com>
2020-04-11 13:04:07 +0200

    sdm660-common: rootdir: migrate tunables from post-boot to init.performance.sdm660
    * Also switched to EAS tunables
    
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -76,6 +76,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.performance.sdm660.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.performance.sdm660.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -56,98 +56,6 @@ else
     echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 fi
 
-echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
-echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
-echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
-echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
-echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
-echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
-
-# Setting b.L scheduler parameters
-echo 96 > /proc/sys/kernel/sched_upmigrate
-echo 90 > /proc/sys/kernel/sched_downmigrate
-echo 140 > /proc/sys/kernel/sched_group_upmigrate
-echo 120 > /proc/sys/kernel/sched_group_downmigrate
-echo 0 > /proc/sys/kernel/sched_select_prev_cpu_us
-echo 400000 > /proc/sys/kernel/sched_freq_inc_notify
-echo 400000 > /proc/sys/kernel/sched_freq_dec_notify
-echo 5 > /proc/sys/kernel/sched_spill_nr_run
-echo 1 > /proc/sys/kernel/sched_restrict_cluster_spill
-echo 100000 > /proc/sys/kernel/sched_short_burst_ns
-echo 1 > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
-echo 20 > /proc/sys/kernel/sched_small_wakee_task_load
-
-# cpuset settings
-echo 0-3 > /dev/cpuset/background/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
-
-# disable thermal bcl hotplug to switch governor
-echo 0 > /sys/module/msm_thermal/core_control/enabled
-
-# online CPU0
-echo 1 > /sys/devices/system/cpu/cpu0/online
-# configure governor settings for little cluster
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
-echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
-echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-echo "85 1747200:95" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-echo 39000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
-echo 633600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/fast_ramp_down
-# online CPU4
-echo 1 > /sys/devices/system/cpu/cpu4/online
-# configure governor settings for big cluster
-echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
-echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
-echo "85 1401600:90 2150400:95" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
-echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-echo 59000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
-echo 1113600 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/fast_ramp_down
-
-# bring all cores online
-echo 1 > /sys/devices/system/cpu/cpu0/online
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
-echo 1 > /sys/devices/system/cpu/cpu4/online
-echo 1 > /sys/devices/system/cpu/cpu5/online
-echo 1 > /sys/devices/system/cpu/cpu6/online
-echo 1 > /sys/devices/system/cpu/cpu7/online
-
-# configure LPM
-echo N > /sys/module/lpm_levels/system/pwr/cpu0/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/pwr/cpu1/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/pwr/cpu2/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/pwr/cpu3/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/cpu4/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/cpu5/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/cpu6/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/cpu7/ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-dynret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/perf-l2-dynret/idle_enabled
-echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-ret/idle_enabled
-echo N > /sys/module/lpm_levels/system/perf/perf-l2-ret/idle_enabled
-# enable LPM
-echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-
-# re-enable thermal and BCL hotplug
-echo 1 > /sys/module/msm_thermal/core_control/enabled
-
 # Set Memory parameters.
 #
 # Set per_process_reclaim tuning parameters
@@ -200,52 +108,9 @@ if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
     echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
 fi
 
-#Set PPR parameters.
-echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
-
-# Set allocstall_threshold to 0 for all targets.
-# Set swappiness to 100 for all targets
-echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-echo 100 > /proc/sys/vm/swappiness
-
 # Disable wsf for all targets beacause we are using efk.
 # wsf Range : 1..1000 So set to bare minimum value 1.
 echo 1 > /proc/sys/vm/watermark_scale_factor
-
-# Enable bus-dcvs
-for cpubw in /sys/class/devfreq/*qcom,cpubw*
-do
-    echo "bw_hwmon" > $cpubw/governor
-    echo 50 > $cpubw/polling_interval
-    echo 762 > $cpubw/min_freq
-    echo "1525 3143 5859 7759 9887 10327 11863 13763" > $cpubw/bw_hwmon/mbps_zones
-    echo 4 > $cpubw/bw_hwmon/sample_ms
-    echo 85 > $cpubw/bw_hwmon/io_percent
-    echo 100 > $cpubw/bw_hwmon/decay_rate
-    echo 50 > $cpubw/bw_hwmon/bw_step
-    echo 20 > $cpubw/bw_hwmon/hist_memory
-    echo 0 > $cpubw/bw_hwmon/hyst_length
-    echo 80 > $cpubw/bw_hwmon/down_thres
-    echo 0 > $cpubw/bw_hwmon/low_power_ceil_mbps
-    echo 34 > $cpubw/bw_hwmon/low_power_io_percent
-    echo 20 > $cpubw/bw_hwmon/low_power_delay
-    echo 0 > $cpubw/bw_hwmon/guard_band_mbps
-    echo 250 > $cpubw/bw_hwmon/up_scale
-    echo 1600 > $cpubw/bw_hwmon/idle_mbps
-done
-
-for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
-do
-    echo "mem_latency" > $memlat/governor
-    echo 10 > $memlat/polling_interval
-    echo 400 > $memlat/mem_latency/ratio_ceil
-done
-echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
 # Start cdsprpcd
 start vendor.cdsprpcd
@@ -253,7 +118,6 @@ start vendor.cdsprpcd
 # Start Host based Touch processing
 case "$hw_platform" in
         "MTP" | "Surf" | "RCM" | "QRD" )
-        start_hbtp
         # Start the Host based Touch processing but not in the power off mode.
         bootmode=`getprop ro.bootmode`
         if [ "charger" != $bootmode ]; then
