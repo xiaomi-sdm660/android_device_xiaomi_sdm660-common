082539f07d374f1e148764f3187c68ad50393e46
Keertesh19 <keertesh.k98@gmail.com>
2020-04-12 18:10:11 +0530

    sdm660-common: Add whyred support
    Change-Id: Id75151a341e0ce495f570c2fba4d07c6d7779ee4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "twolip jasmine_sprout wayne clover lavender platina jason"
+write_headers "twolip jasmine_sprout wayne clover lavender platina jason whyred"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 
