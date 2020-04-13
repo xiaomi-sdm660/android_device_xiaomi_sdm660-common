775b6036398050e8a3a7ed8a5c9cdf2c6d841d21
jimmy <quesoo@gmail.com>
2020-04-13 13:11:37 +0200

    sdm660-common: vndk: add vendor packages needed for camera hal
    Change-Id: I9caef5b5421cec04496b07d33c0f6fbabf0222db

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -16,11 +16,35 @@
 
 EXTRA_VENDOR_LIBRARIES_32 := \
     libandroidicu \
+    libaudioclient \
+    libaudiomanager \
+    libaudiopolicy \
+    libbinder \
+    libbinderthreadstate \
+    libbufferhub \
+    libbufferhubqueue \
+    libcamera_client \
     libft2 \
+    libgui \
     libharfbuzz_ng \
+    libheif \
     libicuuc \
-    libstdc++
+    libicui18n \
+    libinput \
+    libmedia \
+    libmediametrics \
+    libmediautils \
+    libmemunreachable \
+    libminikin \
+    libnblog \
+    libpackagelistparser \
+    libpdx_default_transport \
+    libselinux \
+    libstdc++ \
+    libsonivox \
+    libvibrator
 
 EXTRA_VENDOR_LIBRARIES_64 := \
     libicuuc \
-    libstdc++
+    libstdc++ \
+    libpowermanager
