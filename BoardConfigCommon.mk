3587c05ed8720d1a6d3b6a82fd5ea922198f7880
Stylogey <stylogey@gmail.com>
2020-04-14 17:53:33 +0200

    sdm660-common: use cortex-a73 for arm64
    cortex-a73 memcpy is faster on arm64 while kyro seems to be the best for
    32 bit
    
    Change-Id: Iced75b16aef6f4e1bba5627802759d2924a7dc1c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,7 +36,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := kryo
+TARGET_CPU_VARIANT := cortex-a73
 
 # Architecture 2
 TARGET_2ND_ARCH := arm
