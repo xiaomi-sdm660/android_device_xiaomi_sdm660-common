e7d4c837ba3462d08d5107b37940b092fda2dd9d
Rique Barros <henripaz1998@gmail.com>
2020-04-21 19:41:37 -0300

    platina: use Camera2
    Signed-off-by: Rique Barros <henripaz1998@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    Camera2
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
