2cafb6a59819592f8e6a90d991d14398d96f2ab7
Stylogey <stylogey@gmail.com>
2020-05-05 16:30:12 +0200

    sdm660-common: build radio config 1.1
    Change-Id: I38733cc72284fc984d921ef043001c1c0811a43b

--- sdm660.mk
+++ sdm660.mk
@@ -438,7 +438,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
