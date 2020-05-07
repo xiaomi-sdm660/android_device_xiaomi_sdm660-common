7edb61905637952328ccdcf95b09e62941b55d0f
Stylogey <stylogey@gmail.com>
2020-05-07 09:04:21 +0200

    sdm660-common: use perfconfigstore instead of properties
    Change-Id: I71461329a7ddfc56a124c342e7ac8697e86d69bf

--- common_prop.mk
+++ common_prop.mk
@@ -245,7 +245,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
     persist.vendor.dpm.feature=1 \
     persist.vendor.qcomsysd.enabled=1 \
-    ro.vendor.at_library=libqti-at.so \
     ro.vendor.extension_library=libqti-perfd-client.so
 
 # QTI
--- sdm660.mk
+++ sdm660.mk
@@ -454,7 +454,8 @@ PRODUCT_PACKAGES += \
 
 # QTI Performance
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(COMMON_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # RCS
 PRODUCT_PACKAGES += \
