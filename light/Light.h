f036db8030328e6b17f7790cdba2880b16d57ff5
Demon000 <demonsingur@gmail.com>
2020-05-14 11:44:48 +0200

    sdm660-common: Rework Light HAL based on xiaomi-msm8998 source
    * squash from here
    https://github.com/xiaomi-msm8998/device_xiaomi_msm8998-common/tree/afafb3b3f398cede950eeb369e1f205b2a4d9cf8/light
    
    * Added automatic max brightness detection
    * Rework debug Statments
    
    The xiaomi-sdm660-devs one created Led Light issues
    
    Change-Id: Ie7b62a488770734e659f4db03436ebaa2f2f5f30

--- light/Light.h
+++ light/Light.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2020 The LineageOS Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,35 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
+#define ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
 
 #include <android/hardware/light/2.0/ILight.h>
+#include <hardware/lights.h>
+#include <hidl/Status.h>
+#include <map>
+#include <mutex>
+#include <vector>
 
-#include <unordered_map>
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::light::V2_0::Flash;
+using ::android::hardware::light::V2_0::ILight;
+using ::android::hardware::light::V2_0::LightState;
+using ::android::hardware::light::V2_0::Status;
+using ::android::hardware::light::V2_0::Type;
+
+typedef void (*LightStateHandler)(const LightState&);
+
+struct LightBackend {
+    Type type;
+    LightState state;
+    LightStateHandler handler;
+
+    LightBackend(Type type, LightStateHandler handler) : type(type), handler(handler) {
+        this->state.color = 0xff000000;
+    }
+};
 
 namespace android {
 namespace hardware {
@@ -26,42 +50,13 @@ namespace light {
 namespace V2_0 {
 namespace implementation {
 
-using ::android::hardware::Return;
-using ::android::hardware::light::V2_0::ILight;
-using ::android::hardware::light::V2_0::LightState;
-using ::android::hardware::light::V2_0::Status;
-using ::android::hardware::light::V2_0::Type;
-
 class Light : public ILight {
   public:
-    Light();
-
     Return<Status> setLight(Type type, const LightState& state) override;
     Return<void> getSupportedTypes(getSupportedTypes_cb _hidl_cb) override;
 
   private:
-    void setLightBacklight(Type type, const LightState& state);
-    void setLightButtons(Type type, const LightState& state);
-    void setLightNotification(Type type, const LightState& state);
-    void applyNotificationState(const LightState& state);
-
-    uint32_t max_button_brightness_;
-    uint32_t max_led_brightness_;
-    uint32_t max_red_led_brightness_;
-    uint32_t max_screen_brightness_;
-
-    std::unordered_map<Type, std::function<void(Type type, const LightState&)>> lights_{
-            {Type::BACKLIGHT, [this](auto&&... args) { setLightBacklight(args...); }},
-            {Type::BATTERY, [this](auto&&... args) { setLightNotification(args...); }},
-            {Type::NOTIFICATIONS, [this](auto&&... args) { setLightNotification(args...); }}};
-
-    // Keep sorted in the order of importance.
-    std::array<std::pair<Type, LightState>, 2> notif_states_ = {{
-            {Type::NOTIFICATIONS, {}},
-            {Type::BATTERY, {}},
-    }};
-
-    std::vector<std::string> buttons_;
+    std::mutex globalLock;
 };
 
 }  // namespace implementation
@@ -69,3 +64,5 @@ class Light : public ILight {
 }  // namespace light
 }  // namespace hardware
 }  // namespace android
+
+#endif  // ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
