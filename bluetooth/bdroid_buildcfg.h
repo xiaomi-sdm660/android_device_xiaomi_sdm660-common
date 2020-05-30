2dc852a83c6a86aad1eea051f8d20c427b776de3
Nick C <ctwoon@suicide.today>
2020-05-30 12:50:04 +0300

    sdm660: update bdroid_buildcfg values from caf
    https: //source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf/tree/build/qva/config/bdroid_buildcfg.h?h=LA.UM.8.2.r1-06600-sdm660.0
    Change-Id: I5a95bec3db32eb4f6f83f33087612cf43294fc5b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -51,14 +51,13 @@ static inline const char* BtmGetDefaultName()
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
 
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
-
+#define AVDT_NUM_SEPS 35
 #pragma pop_macro("PROPERTY_VALUE_MAX")
 
 #endif
