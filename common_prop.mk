22e0014387d47d9dc60fd3e51ec5a7b2e668a175
Max Weffers <rcstar6696@gmail.com>
2020-06-02 11:48:14 +0200

    sdm660-common: prop: Set Perf related Props
    *based on sdm845 perf stack with iop
    
    Change-Id: I43a5c721aa53568470e19caadcfed61bf3cbc00e

--- common_prop.mk
+++ common_prop.mk
@@ -231,6 +231,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
 
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+    ro.vendor.at_library=libqti-at.so \
+    vendor.enable.prefetch=0 \
+    vendor.iop.enable_iop=1 \
+    vendor.iop.enable_uxe=1 \
+    vendor.iop.enable_prefetch_ofr=0 \
+    vendor.perf.gestureflingboost.enable=true \
+    vendor.perf.iop_v3.enable=true \
+    vendor.perf.iop_v3.enable.debug=false
+
 # Proximity
 PRODUCT_PROPERTY_OVERRIDES += \
     gsm.proximity.enable=true
@@ -241,8 +254,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
     persist.vendor.dpm.feature=1 \
     persist.vendor.dpm.nsrm.bkg.evt=3955 \
-    persist.vendor.qcomsysd.enabled=1 \
-    ro.vendor.extension_library=libqti-perfd-client.so
+    persist.vendor.qcomsysd.enabled=1
 
 # QTI
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
