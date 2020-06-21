71e85a8f68b0c1a5d2f1d92e7f5d6c4e929ee522
OdSazib <odsazib@gmail.com>
2020-06-22 02:14:10 +0600

    sdm660-common: Drop snap package

--- sdm660.mk
+++ sdm660.mk
@@ -171,8 +171,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
