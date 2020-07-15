a7fd6033cc9b74f21324428c5b9b3d629def125f
OdSazib <odsazib@gmail.com>
2020-07-16 00:46:32 +0600

    sdm660-common: Add powerhint for sdm660 and sdm636 * Add EAS_POWERHINT_VARIANT := sdm636 for whyred in device.mk
    Co-authored-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d

--- sdm660.mk
+++ sdm660.mk
@@ -412,6 +412,15 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage/packages/apps/Snap \
     $(COMMON_PATH)/overlay-system
 
+# Powerhint
+ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
+    PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+else
+    PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+endif
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
