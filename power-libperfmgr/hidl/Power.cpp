ff8b515c1118296de9981bed4e8a886321204375
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-07-15 15:13:39 +0530

    sdm660-common: power-libperfmgr: completely remove camera hints handling
    Change-Id: I1749eb76b0a3aa4afd68fc532b98f1022c4e773a

--- power-libperfmgr/hidl/Power.cpp
+++ power-libperfmgr/hidl/Power.cpp
@@ -52,7 +52,6 @@ Power::Power()
     : mHintManager(nullptr),
       mInteractionHandler(nullptr),
       mSustainedPerfModeOn(false),
-      mCameraStreamingMode(false),
       mReady(false) {
     mInitThread = std::thread([this]() {
         android::base::WaitForProperty(kPowerHalInitProp, "1");
@@ -63,11 +62,7 @@ Power::Power()
         mInteractionHandler = std::make_unique<InteractionHandler>(mHintManager);
         mInteractionHandler->Init();
         std::string state = android::base::GetProperty(kPowerHalStateProp, "");
-        if (state == "CAMERA_STREAMING") {
-            ALOGI("Initialize with CAMERA_STREAMING on");
-            mHintManager->DoHint("CAMERA_STREAMING");
-            mCameraStreamingMode = true;
-        } else if (state == "SUSTAINED_PERFORMANCE") {
+        if (state == "SUSTAINED_PERFORMANCE") {
             ALOGI("Initialize with SUSTAINED_PERFORMANCE on");
             mHintManager->DoHint("SUSTAINED_PERFORMANCE");
             mSustainedPerfModeOn = true;
@@ -195,41 +190,6 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
                 }
             }
             break;
-        case PowerHint_1_2::CAMERA_LAUNCH:
-            if (data > 0) {
-                mHintManager->DoHint("CAMERA_LAUNCH");
-            } else if (data == 0) {
-                mHintManager->EndHint("CAMERA_LAUNCH");
-            } else {
-                ALOGE("CAMERA LAUNCH INVALID DATA: %d", data);
-            }
-            break;
-        case PowerHint_1_2::CAMERA_STREAMING: {
-            if (data > 0) {
-                mHintManager->DoHint("CAMERA_STREAMING");
-                mCameraStreamingMode = true;
-            } else {
-                mHintManager->EndHint("CAMERA_STREAMING");
-                mCameraStreamingMode = false;
-            }
-
-            const auto prop = mCameraStreamingMode
-                                      ? "CAMERA_STREAMING"
-                                      : "";
-            if (!android::base::SetProperty(kPowerHalStateProp, prop)) {
-                ALOGE("%s: could set powerHAL state %s property", __func__, prop);
-            }
-            break;
-        }
-        case PowerHint_1_2::CAMERA_SHOT:
-            if (data > 0) {
-                mHintManager->DoHint("CAMERA_SHOT", std::chrono::milliseconds(data));
-            } else if (data == 0) {
-                mHintManager->EndHint("CAMERA_SHOT");
-            } else {
-                ALOGE("CAMERA SHOT INVALID DATA: %d", data);
-            }
-            break;
         default:
             return powerHint(static_cast<PowerHint_1_0>(hint), data);
     }
@@ -269,10 +229,8 @@ Return<void> Power::debug(const hidl_handle &handle, const hidl_vec<hidl_string>
 
         std::string buf(android::base::StringPrintf(
                 "HintManager Running: %s\n"
-                "CameraStreamingMode: %s\n"
                 "SustainedPerformanceMode: %s\n",
                 boolToString(mHintManager->IsRunning()),
-                boolToString(mCameraStreamingMode),
                 boolToString(mSustainedPerfModeOn)));
         // Dump nodes through libperfmgr
         mHintManager->DumpToFd(fd);
