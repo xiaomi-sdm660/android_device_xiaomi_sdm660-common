87d34fd69040993f2efb91f1fcd260b3e6be9a61
kerneltoast <sultan@kerneltoast.com>
2020-08-01 16:28:24 +0530

    Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.

--- sdm660.mk
+++ sdm660.mk
@@ -603,3 +603,7 @@ PRODUCT_BOOT_JARS += \
 # XiaomiParts
 PRODUCT_PACKAGES += \
    XiaomiParts
+
+# Force triple frame buffers
+PRODUCT_PROPERTY_OVERRIDES += \
+	 ro.surface_flinger.max_frame_buffer_acquired_buffers=3
