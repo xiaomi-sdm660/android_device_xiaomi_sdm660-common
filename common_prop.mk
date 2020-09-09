7566d47ac8a5e4d6e8343997f351da4a4a4193aa
Immanuel Raj <immanuelr44@gmail.com>
2020-09-09 22:18:21 +0530

    Update common_prop.mk
    Added lens switch for gcam without gcam

--- common_prop.mk
+++ common_prop.mk
@@ -135,7 +135,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.eis.enable=1 \
     persist.vendor.camera.exif.make=Xiaomi \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
-    vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord
+    vendor.camera.aux.packageblacklist=com.discord \
+    vendor.camera.aux.packagelist=com.google.android.GoogleCamera,org.codeaurora.snapcam,com.android.camera
 
 # Codec2 switch
 PRODUCT_PROPERTY_OVERRIDES += \
