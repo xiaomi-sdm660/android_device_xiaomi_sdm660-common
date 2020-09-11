5cda1532503be8638c5badec30f981d07d30f37f
Jiyong Park <jiyongp@google.com>
2020-09-11 20:29:04 +0530

    sdm660-common: move VNDK APEX to vendor partition ... for aosp_* targets so that we don't need to update vendor.img everytime there is a change in the VNDK libraries
    Bug: 140136207
    Test: build, m
    Change-Id: Ibfe67b4cca6ef6648ad39a50154cb539348b008a
    
    Signed-off-by: darkbeast69 <guptaaryan189@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -585,7 +585,8 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor_32 \
     libstdc++.vendor_32  \
     vndk-ext \
-    vndk_package
+    vndk_package \
+    com.android.vndk.current.on_vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
