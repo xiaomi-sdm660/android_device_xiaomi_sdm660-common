a99e10643ceddcc4536683805a31c4fe30fe7edc
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-09-11 15:26:28 +0530

    sdm660-common: power-libperfmgr: Adapt and rebrand for xiaomi_sdm660
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- power-libperfmgr/hidl/service.cpp
+++ power-libperfmgr/hidl/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -34,7 +34,7 @@ using android::hardware::power::V1_3::IPower;
 using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char ** /* argv */) {
-    ALOGI("Power HAL Service 1.3 for Pixel is starting.");
+    ALOGI("Power HAL Service 1.3 for Xiaomi SDM660 is starting.");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {
