ecfe2616482bfc8083b038ef552667177d92ed0b
Arian <arian.kulmer@web.de>
2020-09-14 15:33:24 +0530

    sdm660-common: Load vndk 29 libprotobuf for libril-qc-hal-qmi and libwhvdil
    Change-Id: I14e2ceef71541003a452775e1dd7f44c8b3b2b5a
    
    Signed-off-by: darkbeast69 <guptaaryan189@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -587,6 +587,10 @@ PRODUCT_PACKAGES += \
     vndk-ext \
     vndk_package
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
