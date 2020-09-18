0be032e211e3bdd9e87f8e0d970727da25733a2b
OdSazib <odsazib@gmail.com>
2020-09-18 22:33:38 +0600

    sdm660-common: DeviceSettings: Set correct symbol in DreamManager
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- DeviceSettings/src/org/lineageos/settings/device/FPSInfoService.java
+++ DeviceSettings/src/org/lineageos/settings/device/FPSInfoService.java
@@ -282,7 +282,7 @@ public class FPSInfoService extends Service {
 
     private boolean isDozeMode() {
         try {
-            if (mDreamManager != null && mDreamManager.isDozing()) {
+            if (mDreamManager != null && mDreamManager.isDreaming()) {
                 return true;
             }
         } catch (RemoteException e) {
