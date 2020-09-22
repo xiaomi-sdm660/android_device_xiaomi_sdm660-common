b1ecbe5dbf188543785ac933c2bc49ff739c6af4
Harshit Jain <god@hyper-labs.tech>
2020-09-23 04:58:24 +0530

    sdm660-common: define wcg_composition_dataspace as of sunfish
    Change-Id: I94f2a8cb7c4288ee4cad67ce4fa64b68637c89ce

--- common_prop.mk
+++ common_prop.mk
@@ -347,6 +347,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.protected_contents=true
 
 #Storaged
