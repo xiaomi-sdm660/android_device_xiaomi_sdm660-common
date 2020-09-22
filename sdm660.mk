2a90f82af981b603fd861f35cdd27140dc1ab0aa
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sdm660-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed
    
    Signed-off-by: darkbeast69 <guptaaryan189@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -457,7 +457,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
