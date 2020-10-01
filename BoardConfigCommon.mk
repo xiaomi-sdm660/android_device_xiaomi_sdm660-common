8e1fe20ee35da5b134aabb426d4b280b796f1e33
Sap1k <recordyletsplays@gmail.com>
2020-10-01 20:44:11 +0530

    sdm660-common: Temporary ignore Neverallows
    Change-Id: Ie17c06f031d4e474fb7463eb3a6f37c5d4e752c5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -229,6 +229,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
