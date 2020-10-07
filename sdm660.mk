151e86e97bdcfc953807596e06a397dbcd9d29d7
OdSazib <odsazib@gmail.com>
2020-10-07 13:05:26 +0530

    sdm660-common: Set target screen density to 400
    Change-Id: I574b5d322eccc753d37f9fc17599470d12cd9b37

--- sdm660.mk
+++ sdm660.mk
@@ -211,6 +211,9 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
+    
+# Density
+TARGET_SCREEN_DENSITY := 400
 
 # DeviceDoze
 PRODUCT_PACKAGES += \
