4fad1cb9e6eb4920717cde3dd8f0dd76585f4153
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-10-11 06:53:26 +0000

    [DNM] whyred: Introduce 'SafailNet'
    Bypass safetynet even if we have permissive selinux

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -131,3 +131,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
--- sdm660.mk
+++ sdm660.mk
@@ -316,7 +316,8 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.xiaomi_parts.rc \
     init.performance.sdm660.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
