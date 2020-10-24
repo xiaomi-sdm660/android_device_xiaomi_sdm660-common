46027ef09d1be03fe5724adb27f3b37a99f075f4
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-10-24 13:46:12 +0530

    sdm660-common: Enable power-libperfmgr
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -457,7 +457,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
