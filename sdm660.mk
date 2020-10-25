edcd75ed1613241757bc7f850f2892736c5380f0
OdSazib <odsazib@gmail.com>
2020-10-26 02:08:03 +0600

    sdm660-common: Awaken-ify
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -395,6 +395,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
+    $(COMMON_PATH)/overlay-awaken \
     $(COMMON_PATH)/overlay-lineage
 
 # RRO configuration
