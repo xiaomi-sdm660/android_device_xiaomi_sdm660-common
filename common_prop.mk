f68c8f35c179f9d404feb145e10dd5fe2d641ff1
Harshit Jain <god@hyper-labs.tech>
2020-10-29 12:11:18 +0100

    sdm660-common: Update dex2oat config

--- common_prop.mk
+++ common_prop.mk
@@ -6,11 +6,16 @@ endif
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
     dalvik.vm.boot-dex2oat-threads=8 \
-    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-filter=quicken \
     dalvik.vm.dex2oat-threads=8 \
-    dalvik.vm.image-dex2oat-filter=speed \
-    ro.vendor.qti.am.reschedule_service=true
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-threads=8
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
