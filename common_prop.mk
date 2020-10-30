0a02b3003872212939d26b2a8276b54f2f3c35f6
PradyX <pradyumnbhushan@gmail.com>
2020-10-30 16:51:18 +0600

    sdm660-common: Camera package list for aux lens * Fixes google camera aux lense too
    Signed-off-by: odsazib <odsazib@gmail.com>

--- common_prop.mk
+++ common_prop.mk
@@ -131,7 +131,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.eis.enable=1 \
     persist.vendor.camera.exif.make=Xiaomi \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
-    vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord
+    vendor.camera.aux.packageblacklist=com.discord \
+    camera.aux.packagelist=com.google.android.GoogleCamera,com.android.camera,org.codeaurora.snapcam \
+    vendor.camera.aux.packagelist=com.google.android.GoogleCamera,com.android.camera,org.codeaurora.snapcam
 
 # Codec2 switch
 PRODUCT_PROPERTY_OVERRIDES += \
