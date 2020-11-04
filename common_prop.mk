a32adb1d10de7ce2043b19a6f451579d70ce525d
Han Wang <416810799@qq.com>
2020-11-04 17:09:24 +0600

    Revert "sdm660: Enable GL comp backpressure" * This is no-op when debug.sf.disable_backpressure=1    is set. Plus, it lags when it's set alone.    Just follow CAF here and remove this ricing.
    This reverts commit ede4f5c8794a8e94172fd600582909eb410c7d43.
    
    Change-Id: I74c5e4ba830ef23448e4f16822687d7a22746a3b

--- common_prop.mk
+++ common_prop.mk
@@ -154,7 +154,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.enable_gl_backpressure=1 \
     debug.cpurend.vsync=false \
     debug.hwui.use_buffer_age=false \
     persist.debug.wfd.enable=1 \
