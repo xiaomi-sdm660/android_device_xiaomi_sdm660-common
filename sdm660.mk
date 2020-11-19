0d458be010c27e1d5b39548f0f57b27e471c7503
Lunarixus <Nathanb644@hotmail.co.uk>
2020-11-19 11:42:08 +0100

    sdm660-common: Remove packages at build time

--- sdm660.mk
+++ sdm660.mk
@@ -470,6 +470,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
