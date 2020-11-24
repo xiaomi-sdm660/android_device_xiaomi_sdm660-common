0dc4e38c4c08df90cdea4cdcbfac70c8af9a5bc4
Martin <recordyletsplays@gmail.com>
2020-11-24 14:43:48 +0100

    sdm660-common: Decommonize screen density * Not a good idea when we support multiple devices with different screen densities

--- sdm660.mk
+++ sdm660.mk
@@ -214,9 +214,6 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
-    
-# Density
-TARGET_SCREEN_DENSITY := 400
 
 # DeviceDoze
 PRODUCT_PACKAGES += \
