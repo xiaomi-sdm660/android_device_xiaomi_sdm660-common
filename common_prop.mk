01e20206e2d480222b4599227a9d7869ee9ed616
Wang Han <416810799@qq.com>
2020-11-29 07:58:15 +0600

    sdm660-common: Remove custom vsync offsets
     * The values are kanged from hammerhead and may introduce
       latency as it is not selected for any of our panels
       and stock does not set these values in the first place.
    
    Change-Id: Iee854ea77d1e8216c8f01aee2f71a178ffcfaf5c

--- common_prop.mk
+++ common_prop.mk
@@ -317,9 +317,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.protected_contents=true
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
