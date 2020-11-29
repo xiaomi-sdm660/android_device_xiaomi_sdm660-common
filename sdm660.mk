d2f648613112962762d50f4c2f0ff3ffdf64ee27
Michael Bestas <mkbestas@lineageos.org>
2020-11-29 08:11:07 +0600

    sdm660-common: Do not build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- sdm660.mk
+++ sdm660.mk
@@ -559,10 +559,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
