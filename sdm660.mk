4c4d7baf1ab7c698d2ae333cd1caf3fc3a3ddb1a
Manish4586 <manish.n.manish45@gmail.com>
2020-12-29 02:19:37 +0530

    sdm660-common: platform: enable vr high performance options
    * while device on vr the screen flickers enable vr highperformance to able to use "reduce flicker" option.
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: Ibd5622a380523220c4d1c3c4c586aa22e9f30043

--- sdm660.mk
+++ sdm660.mk
@@ -459,7 +459,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
+    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
 
 # Power
 PRODUCT_PACKAGES += \
