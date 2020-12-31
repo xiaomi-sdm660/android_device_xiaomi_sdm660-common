5fbe93dec1e6f9fbb99d933b3b239297b98d0331
Michael Bestas <mkbestas@lineageos.org>
2020-12-31 16:15:18 +0600

    sdm660-common: Move telephony related packages to /system_ext
    Change-Id: I6819c2af9f0f6e0e3823b853277287fc5aa8d504

--- extract-files.sh
+++ extract-files.sh
@@ -74,8 +74,17 @@ fi
 function blob_fixup() {
     case "${1}" in
 
-    product/lib64/libdpmframework.so)
+    system_ext/lib64/libdpmframework.so)
         "$PATCHELF" --add-needed libcutils_shim.so "${2}"
+        ;;    
+    system_ext/etc/init/dpmd.rc)
+        sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
+        ;;
+    system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/telephonyservice.xml)
+        sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
+        ;;
+    system_ext/etc/permissions/qcrilhook.xml)
+        sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
         ;;
 
     vendor/bin/mlipayd@1.1)
