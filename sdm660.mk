ba6bba329d7bb297795659fd19448098d68af0c1
Michael Bestas <mkbestas@lineageos.org>
2020-12-31 17:47:56 +0600

    sdm660-common: Cleanup dead targets
    Change-Id: Ib1e2965a5e9f16929f1dc11c719d917cd85604d1

--- sdm660.mk
+++ sdm660.mk
@@ -88,11 +88,9 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl\
     audio.a2dp.default \
-    audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
@@ -131,7 +129,6 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    antradio_app \
     com.dsi.ant.antradio_library
 
 # ANT Permission
