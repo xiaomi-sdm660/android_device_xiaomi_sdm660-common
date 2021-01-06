15f8321151f45e5183e7cbb324b74bd014006feb
etahamad <etahamad@icloud.com>
2021-01-06 02:55:45 +0200

    sdm660-common: Add required GPU props
    These tell the ROM to use the correct Adreno drivers which fixes graphical artifacts.
    
    Credit to Sultan for the triple buffers prop.
    
    Credit to kdrag0n for noticing vulkan prop was missing.
    
    Change-Id: I3481092f3aea2be2091c8c64ca04c426a244a696
    
    Co-authored-by: Lunarixus <Nathan@lunarixus.party>

--- common_prop.mk
+++ common_prop.mk
@@ -188,6 +188,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.min.fling_velocity=160 \
     ro.max.fling_velocity=20000
 
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno
+
 # GMS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase.ms=android-xiaomi-rev2
