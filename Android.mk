e4948702d113a5083e150440a85a7a8f855bc588
Alexander Koskovich <zvnexus@outlook.com>
2021-01-25 19:09:14 +0100

    sdm660-common: Add missing egl symlinks
    Change-Id: Iab9b801e24edeba16dd5a91a025daf7216684040

--- Android.mk
+++ Android.mk
@@ -80,6 +80,25 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+EGL_LIBRARIES := \
+	libEGL_adreno.so \
+	libGLESv2_adreno.so \
+	libq3dtools_adreno.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf egl/$(notdir $@) $@
+
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"
@@ -117,18 +136,11 @@ $(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
 
 ALL_DEFAULT_INSTALLED_MODULES += \
-    $(RFS_MSM_ADSP_SYMLINKS) \
-    $(RFS_MSM_MPSS_SYMLINKS) \
-    $(RFS_MSM_SLPI_SYMLINKS)
-
-EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so \
-    $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so \
-    $(TARGET_OUT_VENDOR)/lib64/libEGL_adreno.so
-$(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS)
+	$(EGL_32_SYMLINKS) \
+	$(EGL_64_SYMLINKS) \
+	$(RFS_MSM_ADSP_SYMLINKS) \
+	$(RFS_MSM_MPSS_SYMLINKS) \
+	$(RFS_MSM_SLPI_SYMLINKS)
 
 METADATA_SYMLINKS := $(TARGET_ROOT_OUT)/metadata
 $(METADATA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
