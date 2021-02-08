038b1691dd5df836bb25022af2cd7d2f8b7b8a18
Immanuel Raj <immanuelr44@gmail.com>
2021-02-08 16:41:39 +0530

    sdm660-common: Stop building gapps
    This can be called from dt depending on rom so this being here sometimes cause problems and makes no sense

--- sdm660.mk
+++ sdm660.mk
@@ -40,7 +40,6 @@ endif
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 
 # Common Tree Path
 COMMON_PATH := device/xiaomi/sdm660-common
