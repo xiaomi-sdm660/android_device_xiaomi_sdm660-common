851d88070eeeac5e5e3adb8ef352ddc06e35c477
Chenyang Zhong <zhongcy95@gmail.com>
2021-02-09 11:04:23 +0600

    sdm660-common: create and symlink a dummy /metadata/apex
    Google moved apex sessions directory from /data/apex/sessions to
    /metadata/apex/sessions after commit:
    
    "Move apex sessions directory to /metadata"
    https://android.googlesource.com/platform/system/apex/+/36cf4bbac6108898a531aacc102dcc489d791b37
    
    Devices with a mounted metadata partition will have the needed
    directories set up by system/core/rootdir/init.rc. Xiaomi devices
    on sm6125 do not have a metadata partition out of the box, so things
    like "Google Play system update" will fail to install the update.
    
    Therefore, create a dummy directory under /data/vendor/metadata_apex
    and symlink it to /metadata/apex.
    
    The reason why the old /data/apex/sessions directory is not used
    for the symlink is that apexd will call migrateSessionsDirIfNeeded()
    to recursively copy things from the old directory to the new one.
    Creating the symlink from /data/apex/sessions may result in
    unintended behaviors.
    
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- Android.mk
+++ Android.mk
@@ -130,6 +130,14 @@ $(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS)
 
+METADATA_SYMLINK := $(TARGET_ROOT_OUT)/metadata
+$(METADATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $@"
+	@mkdir -p $(TARGET_ROOT_OUT)/metadata
+	$(hide) ln -sf /data/vendor/metadata_apex $@/apex
+
+ALL_DEFAULT_INSTALLED_MODULES += $(METADATA_SYMLINK)
+
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
