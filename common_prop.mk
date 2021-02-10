53b5171fc64809e024084475e2abe87f94f1c4bc
etahamad <etahamad@icloud.com>
2021-02-10 11:33:20 +0200

    sdm660-common: Handle vendor.audio.adm.buffering.ms for lavender
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- common_prop.mk
+++ common_prop.mk
@@ -42,7 +42,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.audio.fluence.voicecomm=true \
     ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.adm.buffering.ms=12 \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
     vendor.audio.flac.sw.decoder.24bit=true \
@@ -116,6 +115,15 @@ else
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.spkr_prot.enable=true
 endif
+
+ifneq ($(filter lavender,$(TARGET_DEVICE)),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.adm.buffering.ms=12
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.adm.buffering.ms=6
+endif
+
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
