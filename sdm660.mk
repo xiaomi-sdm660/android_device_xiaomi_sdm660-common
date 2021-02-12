feeeb37c8226d3d0398047b9294c05ae0d56443e
SakilMondal <mondalsakil@gmail.com>
2021-02-12 12:16:23 +0800

    sdm660-common: add QTI health implementation Ref: https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/healthd-ext/commit/?h=android-core.lnx.6.0.r7-rel&id=995c63a75db6366c28b3a4c1dcb00a77cba090d4 Change-Id: Ib4406e22626f7c0a85a3c9a70d5bea3fc917f9c1

--- sdm660.mk
+++ sdm660.mk
@@ -262,7 +262,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
 # HIDL
