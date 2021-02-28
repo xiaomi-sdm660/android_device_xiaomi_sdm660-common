d7334ac1eb5280b65c20d34df52febce1dd557e8
sbwml <984419930@qq.com>
2021-03-01 01:05:51 +0600

    sdm660-common: Set loop max part to 7 loop.max_part must be less than 8, otherwise twrp generates warning with endless loop for apex
    Reference: https://source.android.com/devices/tech/ota/apex#required_kernel_configuration_options

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,7 +151,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm660
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += loop.max_part=16
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
