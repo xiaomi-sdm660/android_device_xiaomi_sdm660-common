4aec2ab1c3d95eb899ee74041fc8a76a7000fca0
Manish4586 <manish.n.manish45@gmail.com>
2021-03-01 21:09:34 +0530

    sdm660-common: audio: Switch to audio hal v4

--- sdm660.mk
+++ sdm660.mk
@@ -90,8 +90,9 @@ endif
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     audio.a2dp.default \
     audio.primary.sdm660 \
