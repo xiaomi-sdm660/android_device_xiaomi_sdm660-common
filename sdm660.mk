d31c630682bd3d377047d604c7555f192f98a7d2
OdSazib <odsazib@gmail.com>
2021-03-10 03:38:58 +0600

    sdm660-common: Fix sensorservice

--- sdm660.mk
+++ sdm660.mk
@@ -504,6 +504,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
