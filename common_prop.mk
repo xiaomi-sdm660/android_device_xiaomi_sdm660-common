9e6a31ceb06246d008bf9678038c3dad5edf6b02
Rahul Krishna <Rahulkrishna585@gmail.com>
2021-03-31 07:16:26 +0600

    sdm660-common: Disable GPU protected composition * Causes reboot while trying to play videos on Netflix, HOOQ etc.

--- common_prop.mk
+++ common_prop.mk
@@ -273,7 +273,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.use_smart_90_for_video=true \
     ro.surface_flinger.set_display_power_timer_ms=10000 \
     ro.surface_flinger.set_touch_timer_ms=5000 \
