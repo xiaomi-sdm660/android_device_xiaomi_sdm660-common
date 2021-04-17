12bff7aad4d1b6e59ec8527804b3f5e237181cbe
OdSazib <odsazib@gmail.com>
2021-04-17 23:30:26 +0600

    sdm660-common: sepolicy: Rework sepolicy (No more neverallow) - Thanks to LineageOS and our sdm660 community
    Change-Id: I54c7d76260041b7c383428449e149aa35d51de9b3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -222,7 +222,6 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
