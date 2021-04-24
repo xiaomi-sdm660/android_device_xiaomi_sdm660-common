25c653ba0b06af9f9a532fa32d8dca9cd4ef13ef
OdSazib <odsazib@gmail.com>
2021-04-24 19:42:45 +0600

    sdm660-common: audio: Restore audio hal v6.0
    This reverts commit 4aec2ab1c3d95eb899ee74041fc8a76a7000fca0.

--- sdm660.mk
+++ sdm660.mk
@@ -94,8 +94,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     audio.a2dp.default \
