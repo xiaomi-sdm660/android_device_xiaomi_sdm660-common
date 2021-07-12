b8d7f9ddfc4c0a1c663dd83ec33e08c3e64925e5
OdSazib <odsazib@gmail.com>
2021-07-12 08:23:42 +0600

    Revert "sdm660-common: Enable AOSP surfaceflinger"
    This reverts commit dabf68b62af88b0d8d8afa0c22a03d94fdce8c83

--- properties/common_prop.mk
+++ properties/common_prop.mk
@@ -273,14 +273,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.pmd=true \
     ro.vendor.sensors.sta_detect=true
 
-# SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.use_smart_90_for_video=true \
-    ro.surface_flinger.set_display_power_timer_ms=10000 \
-    ro.surface_flinger.set_touch_timer_ms=5000 \
-    ro.surface_flinger.set_idle_timer_ms=9000 \
-    ro.surface_flinger.wcg_composition_dataspace=143261696
-
 # System restart
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.shutdown_timeout=2
